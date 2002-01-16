e6d94a372b735da79d9aba8a8fc503559c0d57a0
lordhavoc <lordhavoc@d7cf8633-e32d-0410-b094-e92efae38249>
2002-01-16 06:20:30 +0000

    forgot to remove these
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@1344 d7cf8633-e32d-0410-b094-e92efae38249

--- r_decals.h
+++
@@ -1,6 +0,0 @@
-
-void R_Decals_Init(void);
-
-void GL_DrawDecals(void);
-
-void R_Decal(vec3_t org, rtexture_t *tex, float s1, float t1, float s2, float t2, float scale, int cred, int cgreen, int cblue, int alpha);
