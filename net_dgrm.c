2125216fec3456e8c80797a1fb8bb26ed244e352
lordhavoc <lordhavoc@d7cf8633-e32d-0410-b094-e92efae38249>
2002-03-16 14:49:13 +0000

    whitespace
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@1657 d7cf8633-e32d-0410-b094-e92efae38249

--- net_dgrm.c
+++ net_dgrm.c
@@ -323,7 +323,7 @@ int	Datagram_GetMessage (qsocket_t *sock)
 			ReSendMessage (sock);
 
 	while(1)
-	{	
+	{
 		length = sfunc.Read (sock->socket, (qbyte *)&packetBuffer, NET_DATAGRAMSIZE, &readaddr);
 
 //	if ((rand() & 255) > 220)
