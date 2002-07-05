444342309adca329b3483ee873c14c874c9442e5
knghtbrd <knghtbrd@d7cf8633-e32d-0410-b094-e92efae38249>
2002-07-05 03:28:10 +0000

    Kill trigraphs.  Die die die.  Or something.
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@1983 d7cf8633-e32d-0410-b094-e92efae38249

--- pr_edict.c
+++ pr_edict.c
@@ -500,7 +500,7 @@ char *PR_GlobalString (int ofs)
 	val = (void *)&pr_globals[ofs];
 	def = ED_GlobalAtOfs(ofs);
 	if (!def)
-		sprintf (line,"%i(\?\?\?)", ofs); // LordHavoc: escaping the ?s so it is not a trigraph
+		sprintf (line,"%i(?)", ofs);
 	else
 	{
 		s = PR_ValueString (def->type, val);
@@ -523,7 +523,7 @@ char *PR_GlobalStringNoContents (int ofs)
 
 	def = ED_GlobalAtOfs(ofs);
 	if (!def)
-		sprintf (line,"%i(\?\?\?)", ofs); // LordHavoc: escaping the ?s so it is not a trigraph
+		sprintf (line,"%i(?)", ofs);
 	else
 		sprintf (line,"%i(%s)", ofs, pr_strings + def->s_name);
 
