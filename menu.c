7a8760e2510c377944833b4e40a16ebdd064e64e
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2002-08-28 12:14:11 +0000

    added lighting options (and explosion clipping) to effects options menu
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@2296 d7cf8633-e32d-0410-b094-e92efae38249

--- menu.c
+++ menu.c
@@ -1481,7 +1481,7 @@ void M_Options_Key (int k)
 	}
 }
 
-#define	OPTIONS_EFFECTS_ITEMS	12
+#define	OPTIONS_EFFECTS_ITEMS	16
 
 int options_effects_cursor;
 
@@ -1497,7 +1497,11 @@ extern cvar_t r_detailtextures;
 extern cvar_t cl_particles;
 extern cvar_t cl_explosions;
 extern cvar_t cl_stainmaps;
+extern cvar_t r_explosionclip;
+extern cvar_t r_dlightmap;
 extern cvar_t r_modellights;
+extern cvar_t r_coronas;
+extern cvar_t gl_flashblend;
 extern cvar_t cl_particles_bulletimpacts;
 extern cvar_t cl_particles_smoke;
 extern cvar_t cl_particles_sparks;
@@ -1516,36 +1520,48 @@ void M_Menu_Options_Effects_AdjustSliders (int dir)
 		Cvar_SetValueQuick (&r_modellights, bound(0, r_modellights.value + dir, 8));
 		break;
 	case 1:
-		Cvar_SetValueQuick (&cl_particles, !cl_particles.integer);
+		Cvar_SetValueQuick (&r_dlightmap, !r_dlightmap.integer);
 		break;
 	case 2:
-		Cvar_SetValueQuick (&cl_explosions, !cl_explosions.integer);
+		Cvar_SetValueQuick (&r_coronas, !r_coronas.integer);
 		break;
 	case 3:
-		Cvar_SetValueQuick (&cl_stainmaps, !cl_stainmaps.integer);
+		Cvar_SetValueQuick (&gl_flashblend, !gl_flashblend.integer);
 		break;
 	case 4:
-		Cvar_SetValueQuick (&r_detailtextures, !r_detailtextures.integer);
+		Cvar_SetValueQuick (&cl_particles, !cl_particles.integer);
 		break;
 	case 5:
-		Cvar_SetValueQuick (&cl_particles_bulletimpacts, !cl_particles_bulletimpacts.integer);
+		Cvar_SetValueQuick (&cl_explosions, !cl_explosions.integer);
 		break;
 	case 6:
-		Cvar_SetValueQuick (&cl_particles_smoke, !cl_particles_smoke.integer);
+		Cvar_SetValueQuick (&r_explosionclip, !r_explosionclip.integer);
 		break;
 	case 7:
-		Cvar_SetValueQuick (&cl_particles_sparks, !cl_particles_sparks.integer);
+		Cvar_SetValueQuick (&cl_stainmaps, !cl_stainmaps.integer);
 		break;
 	case 8:
-		Cvar_SetValueQuick (&cl_particles_bubbles, !cl_particles_bubbles.integer);
+		Cvar_SetValueQuick (&r_detailtextures, !r_detailtextures.integer);
 		break;
 	case 9:
-		Cvar_SetValueQuick (&cl_particles_blood, !cl_particles_blood.integer);
+		Cvar_SetValueQuick (&cl_particles_bulletimpacts, !cl_particles_bulletimpacts.integer);
 		break;
 	case 10:
-		Cvar_SetValueQuick (&cl_particles_blood_size, bound(2, cl_particles_blood_size.value + dir * 1, 20));
+		Cvar_SetValueQuick (&cl_particles_smoke, !cl_particles_smoke.integer);
 		break;
 	case 11:
+		Cvar_SetValueQuick (&cl_particles_sparks, !cl_particles_sparks.integer);
+		break;
+	case 12:
+		Cvar_SetValueQuick (&cl_particles_bubbles, !cl_particles_bubbles.integer);
+		break;
+	case 13:
+		Cvar_SetValueQuick (&cl_particles_blood, !cl_particles_blood.integer);
+		break;
+	case 14:
+		Cvar_SetValueQuick (&cl_particles_blood_size, bound(2, cl_particles_blood_size.value + dir * 1, 20));
+		break;
+	case 15:
 		Cvar_SetValueQuick (&cl_particles_blood_alpha, bound(0.2, cl_particles_blood_alpha.value + dir * 0.1, 1));
 		break;
 	}
@@ -1562,8 +1578,12 @@ void M_Options_Effects_Draw (void)
 
 	y = 32;
 	M_Print(16, y, "      Lights Per Model");M_DrawSlider(220, y, r_modellights.value / 8);y += 8;
+	M_Print(16, y, " Fast Dynamic Lighting");M_DrawCheckbox(220, y, !r_dlightmap.integer);y += 8;
+	M_Print(16, y, "               Coronas");M_DrawCheckbox(220, y, r_coronas.integer);y += 8;
+	M_Print(16, y, "      Use Only Coronas");M_DrawCheckbox(220, y, gl_flashblend.integer);y += 8;
 	M_Print(16, y, "             Particles");M_DrawCheckbox(220, y, cl_particles.integer);y += 8;
 	M_Print(16, y, "            Explosions");M_DrawCheckbox(220, y, cl_explosions.integer);y += 8;
+	M_Print(16, y, "    Explosion Clipping");M_DrawCheckbox(220, y, r_explosionclip.integer);y += 8;
 	M_Print(16, y, "             Stainmaps");M_DrawCheckbox(220, y, cl_stainmaps.integer);y += 8;
 	M_Print(16, y, "      Detail Texturing");M_DrawCheckbox(220, y, r_detailtextures.integer);y += 8;
 	M_Print(16, y, "        Bullet Impacts");M_DrawCheckbox(220, y, cl_particles_bulletimpacts.integer);y += 8;
