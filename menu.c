78da49ba852ed63b1079cc1ab8545e82c4abd78e
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2002-09-23 04:20:04 +0000

    no need to include winquake.h
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@2419 d7cf8633-e32d-0410-b094-e92efae38249

--- menu.c
+++ menu.c
@@ -19,10 +19,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 #include "quakedef.h"
 
-#ifdef _WIN32
-#include "winquake.h"
-#endif
-
 void (*vid_menudrawfn)(void);
 void (*vid_menukeyfn)(int key);
 
