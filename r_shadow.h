2fd062546c9885bcf8e6e5a4eb6e6bbc06361358
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2002-09-30 02:07:32 +0000

    forgot to commit this
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@2478 d7cf8633-e32d-0410-b094-e92efae38249

---
+++ r_shadow.h
@@ -0,0 +1,8 @@
+
+#ifndef R_SHADOW_H
+#define R_SHADOW_H
+
+void R_Shadow_Init(void);
+void R_ShadowVolume(int numverts, int numtris, int *elements, int *neighbors, vec3_t relativelightorigin, float projectdistance);
+
+#endif
