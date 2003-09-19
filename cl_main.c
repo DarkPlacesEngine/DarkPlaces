bb50aa2d2b24eecae3d44f98c3e88c04f9ede403
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2003-09-19 01:52:41 +0000

    finished a lot of little todo items, mostly regarding server list and networking init bugs, and fixed skybox rendering
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@3469 d7cf8633-e32d-0410-b094-e92efae38249

--- cl_main.c
+++ cl_main.c
@@ -222,13 +222,13 @@ void CL_EstablishConnection(const char *host)
 
 	// clear menu's connect error message
 	m_return_reason[0] = 0;
-
-	// stop demo loop in case this fails
 	cls.demonum = -1;
-	CL_Disconnect();
 
 	if (LHNETADDRESS_FromString(&cls.connect_address, host, 26000) && (cls.connect_mysocket = NetConn_ChooseClientSocketForAddress(&cls.connect_address)))
 	{
+		// stop demo loop in case this fails
+		CL_Disconnect();
+
 		cls.connect_trying = true;
 		cls.connect_remainingtries = 3;
 		cls.connect_nextsendtime = 0;
@@ -243,6 +243,11 @@ void CL_EstablishConnection(const char *host)
 			NetConn_ClientFrame();
 			NetConn_ServerFrame();
 		}
+	}
+	else
+	{
+		Con_Printf("Unable to find a suitable network socket to connect to server.\n");
+		strcpy(m_return_reason, "No network");
 	}
 }
 
