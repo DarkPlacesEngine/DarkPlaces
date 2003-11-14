1ded652d4ef15c4b64a1d82e01ed82c6e568d35b
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2003-11-14 13:46:28 +0000

    fix a couple signed/unsigned comparison warnings
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@3644 d7cf8633-e32d-0410-b094-e92efae38249

--- menu.c
+++ menu.c
@@ -2147,7 +2147,7 @@ int video_cursor_table[] = {56, 68, 80, 100};
 // note: if modes are added to the beginning of this list, update the
 // video_resolution = x; in M_Menu_Video_f below
 unsigned short video_resolutions[][2] = {{320,240}, {400,300}, {512,384}, {640,480}, {800,600}, {1024,768}, {1152,864}, {1280,960}, {1280,1024}, {1600,1200}, {1792,1344}, {1920,1440}, {2048,1536}, {0,0}};
-#define VID_RES_COUNT ((int)sizeof(video_resolutions) / sizeof(video_resolutions[0]) - 1)
+#define VID_RES_COUNT ((int)(sizeof(video_resolutions) / sizeof(video_resolutions[0])) - 1)
 int video_resolution;
 
 extern int current_vid_fullscreen;
