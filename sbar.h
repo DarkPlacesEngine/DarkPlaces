2293884f172ebd00c2030ec4a40273aaa74045a1
black <black@d7cf8633-e32d-0410-b094-e92efae38249>
2003-12-20 18:32:47 +0000

    Added the cvar sbar_alpha.
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@3746 d7cf8633-e32d-0410-b094-e92efae38249

--- sbar.h
+++ sbar.h
@@ -24,6 +24,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define	SBAR_HEIGHT		24
 
 extern	int			sb_lines;			// scan lines to draw
+extern	cvar_t		sbar_alpha;
 
 void Sbar_Init (void);
 
