a40195d9f77dc041976c4a130f5bf8fb69dbc181
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2004-02-02 14:55:05 +0000

    fix misplaced extern S_UnblockSound
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@3864 d7cf8633-e32d-0410-b094-e92efae38249

--- sys_win.c
+++ sys_win.c
@@ -28,7 +28,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "direct.h"
 
 extern void S_BlockSound (void);
-extern void S_UnblockSound (void);
 
 cvar_t sys_usetimegettime = {CVAR_SAVE, "sys_usetimegettime", "1"};
 
--- vid_wgl.c
+++ vid_wgl.c
@@ -26,6 +26,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <commctrl.h>
 
 extern void S_BlockSound (void);
+extern void S_UnblockSound (void);
 extern HINSTANCE global_hInstance;
 
 
