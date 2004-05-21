c26c3c9f7aab77b258ff04e28e9e5d7362450a61
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2004-05-21 18:55:30 +0000

    Tomaz renamed cl_fake* networking cvars to cl_net*
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@4217 d7cf8633-e32d-0410-b094-e92efae38249

--- lhnet.c
+++ lhnet.c
@@ -674,8 +674,8 @@ int LHNET_Write(lhnetsocket_t *lhnetsocket, const void *content, int contentleng
 		p->next->prev = p;
 		p->prev->next = p;
 #ifndef STANDALONETEST
-		if (cl_fakelocalping_min.value || cl_fakelocalping_max.value)
-			p->sentdoubletime = Sys_DoubleTime() + lhrandom(cl_fakelocalping_min.value, cl_fakelocalping_max.value) * (0.5 / 1000.0);
+		if (cl_netlocalping_min.value || cl_netlocalping_max.value)
+			p->sentdoubletime = Sys_DoubleTime() + lhrandom(cl_netlocalping_min.value, cl_netlocalping_max.value) * (0.5 / 1000.0);
 #endif
 		value = contentlength;
 	}
