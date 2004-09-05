79f62acfcc23a9597c6080a42834aff1c3cfebd8
tomaz <tomaz@d7cf8633-e32d-0410-b094-e92efae38249>
2004-09-05 11:18:49 +0000

    Added two cvars (sbar_alpha_bg and sbar_alpha_fg) to replace sbar_alpha, one controls background as 0-1, and one controls everything else as 0-1
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@4437 d7cf8633-e32d-0410-b094-e92efae38249

--- sbar.h
+++ sbar.h
@@ -24,7 +24,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #define	SBAR_HEIGHT		24
 
 extern	int			sb_lines;			// scan lines to draw
-extern	cvar_t		sbar_alpha;
+extern	cvar_t		sbar_alpha_bg;
+extern	cvar_t		sbar_alpha_fg;
 
 void Sbar_Init (void);
 
