bebc27240df9f226f7968e3dafd7f669e715681b
molivier <molivier@d7cf8633-e32d-0410-b094-e92efae38249>
2004-10-13 13:45:16 +0000

    Fixed a couple of comments, and modified slightly the developer printing in ResampleSfx
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@4626 d7cf8633-e32d-0410-b094-e92efae38249

--- snd_mix.c
+++ snd_mix.c
@@ -17,7 +17,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 */
-// snd_mix.c -- portable code to mix sounds for snd_dma.c
+// snd_mix.c -- portable code to mix sounds
 
 #include "quakedef.h"
 #include "snd_main.h"
