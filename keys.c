13e8c23d8ae30c2cece331a47e80176ea8a4f6cf
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2005-02-11 22:20:42 +0000

    removed key-below-escape forced toggleconsole, so that german keyboards can type ^
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@5014 d7cf8633-e32d-0410-b094-e92efae38249

--- keys.c
+++ keys.c
@@ -14,7 +14,7 @@
 
 	You should have received a copy of the GNU General Public License
 	along with this program; if not, write to:
-	
+
 		Free Software Foundation, Inc.
 		59 Temple Place - Suite 330
 		Boston, MA  02111-1307, USA
@@ -908,14 +908,6 @@ Key_Event (int key, char ascii, qboolean down)
 				else
 					Sys_Error ("Bad key_dest");
 		}
-		return;
-	}
-
-	// console key is hardcoded, so the user can never unbind it
-	if (key == '`' || key == '~')
-	{
-		if (down)
-			Con_ToggleConsole_f ();
 		return;
 	}
 
