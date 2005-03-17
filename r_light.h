ea1a30a140048d0ef4c9023d4804bfa57eba9fa8
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2005-03-17 07:21:18 +0000

    removed a lot of renderer cruft (such as 48% of gl_rsurf.c) as a result of the previous commit
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@5090 d7cf8633-e32d-0410-b094-e92efae38249

--- r_light.h
+++ r_light.h
@@ -6,9 +6,6 @@ extern int r_numdlights;
 extern dlight_t r_dlight[MAX_DLIGHTS];
 
 void R_UpdateLights(void);
-#ifdef LHREMOVESOON
-void R_MarkLights(entity_render_t *ent);
-#endif
 void R_DrawCoronas(void);
 void R_CompleteLightPoint(vec3_t ambientcolor, vec3_t diffusecolor, vec3_t diffusenormal, const vec3_t p, int dynamic, const mleaf_t *leaf);
 int R_LightModel(float *ambient4f, float *diffusecolor, float *diffusenormal, const entity_render_t *ent, float colorr, float colorg, float colorb, float colora, int worldcoords);
