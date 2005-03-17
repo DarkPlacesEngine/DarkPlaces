955dca6929a19372ccbe2eee07191949ffac5dc2
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2005-03-17 07:21:18 +0000

    removed a lot of renderer cruft (such as 48% of gl_rsurf.c) as a result of the previous commit
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@5090 d7cf8633-e32d-0410-b094-e92efae38249

--- model_brush.h
+++ model_brush.h
@@ -181,13 +181,6 @@ typedef struct msurface_s
 	int extents[2]; // q1bsp
 	// if lightmap settings changed, this forces update
 	int cached_dlight; // q1bsp
-#ifdef LHREMOVESOON
-	// if this == r_framecount there are dynamic lights on the surface
-	int dlightframe; // q1bsp
-	// which dynamic lights are touching this surface
-	// (only access this if dlightframe is current)
-	int dlightbits[8]; // q1bsp
-#endif
 
 	struct q3deffect_s *effect; // q3bsp
 	// FIXME: collisionmarkframe should be kept in a separate array
