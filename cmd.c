fa0548ee00d1487dc4c1b76072cc42b49801b16f
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2005-05-06 20:01:55 +0000

    hush "Unknown command" warnings during startup
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@5240 d7cf8633-e32d-0410-b094-e92efae38249

--- cmd.c
+++ cmd.c
@@ -840,7 +840,7 @@ void Cmd_ExecuteString (const char *text, cmd_source_t src)
 	}
 
 // check cvars
-	if (!Cvar_Command ())
+	if (!Cvar_Command () && host_framecount > 0)
 		Con_Printf("Unknown command \"%s\"\n", Cmd_Argv(0));
 
 	cmd_tokenizebufferpos = oldpos;
