8df4533747c725948afde838b13441c8146d7f45
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2005-07-05 13:02:21 +0000

    added a #define DIRECTINPUT_VERSION to hush warnings and make it work on dinput3 even if dinput8 headers are installed
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@5506 d7cf8633-e32d-0410-b094-e92efae38249

--- vid_wgl.c
+++ vid_wgl.c
@@ -19,6 +19,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 // gl_vidnt.c -- NT GL vid component
 
+// we don't need a very new dinput
+#define DIRECTINPUT_VERSION 0x0300
+
 #include "quakedef.h"
 #include <windows.h>
 #include <dsound.h>
