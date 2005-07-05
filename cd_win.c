bc72b2af9cc6e8819e80aa96e7aa15762ce6ff16
black <black@d7cf8633-e32d-0410-b094-e92efae38249>
2005-07-05 13:01:29 +0000

    fix VC 6 not supporting DWORD_PTR.
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@5505 d7cf8633-e32d-0410-b094-e92efae38249

--- cd_win.c
+++ cd_win.c
@@ -25,6 +25,9 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "quakedef.h"
 #include "cdaudio.h"
 
+#if _MSC_VER < 1300
+typedef DWORD DWORD_PTR;
+#endif
 
 extern	HWND	mainwindow;
 
