e8251b8a7c2e979388b83d7c982ce36ab7c4ed4b
sajt <sajt@d7cf8633-e32d-0410-b094-e92efae38249>
2005-08-25 02:07:05 +0000

    Get client velocity from cl.velocity rather than cl.movement_velocity for bob, this makes demos use the bob too
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@5657 d7cf8633-e32d-0410-b094-e92efae38249

--- view.c
+++ view.c
@@ -417,7 +417,7 @@ void V_CalcRefdef (void)
 				{
 					double xyspeed, bob;
 
-					xyspeed = sqrt(cl.movement_velocity[0]*cl.movement_velocity[0] + cl.movement_velocity[1]*cl.movement_velocity[1]);
+					xyspeed = sqrt(cl.velocity[0]*cl.velocity[0] + cl.velocity[1]*cl.velocity[1]);
 					if (cl_bob.value && cl_bobcycle.value)
 					{
 						float cycle;
