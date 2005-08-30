caaae17ef797cc84b7ee83ff546fb5abf5d29ea4
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2005-08-30 08:53:01 +0000

    modified Mod_ValidateElements to fix broken elements rather than crashing later
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@5672 d7cf8633-e32d-0410-b094-e92efae38249

--- model_shared.h
+++ model_shared.h
@@ -559,7 +559,7 @@ extern char loadname[32];	// for hunk tags
 
 int Mod_BuildVertexRemapTableFromElements(int numelements, const int *elements, int numvertices, int *remapvertices);
 void Mod_BuildTriangleNeighbors(int *neighbors, const int *elements, int numtriangles);
-void Mod_ValidateElements(const int *elements, int numtriangles, int numverts, const char *filename, int fileline);
+void Mod_ValidateElements(int *elements, int numtriangles, int numverts, const char *filename, int fileline);
 void Mod_BuildNormals(int firstvertex, int numvertices, int numtriangles, const float *vertex3f, const int *elements, float *normal3f, qboolean areaweighting);
 void Mod_BuildTextureVectorsAndNormals(int firstvertex, int numvertices, int numtriangles, const float *vertex, const float *texcoord, const int *elements, float *svectors, float *tvectors, float *normals, qboolean areaweighting);
 
