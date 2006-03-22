8bf0a0e63b2c9998aa79491da2f31fd6d73dd49c
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2006-03-22 09:18:03 +0000

    another WIN32_LEAN_AND_MEAN patch from Willis, this one for non-SDL client and dedicated server
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@6168 d7cf8633-e32d-0410-b094-e92efae38249

--- cd_win.c
+++ cd_win.c
@@ -21,6 +21,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // rights reserved.
 
 #include <windows.h>
+#include <mmsystem.h>
 
 #include "quakedef.h"
 #include "cdaudio.h"
