6e016512263903cf98ab897e59a5267b4bf0688d
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2006-03-28 03:01:52 +0000

    added two more psk/psa versions reported by Kazashi
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@6196 d7cf8633-e32d-0410-b094-e92efae38249

--- model_alias.c
+++ model_alias.c
@@ -1798,8 +1798,8 @@ void Mod_PSKMODEL_Load(model_t *mod, void *buffer, void *bufferend)
 		numrecords = LittleLong(pchunk->numrecords);
 		if (developer.integer >= 100)
 			Con_Printf("%s: %s %x: %i * %i = %i\n", loadmodel->name, pchunk->id, version, recordsize, numrecords, recordsize * numrecords);
-		if (version != 0x1e83b9 && version != 0x1e9179 && version != 0x2e)
-			Con_Printf ("%s: chunk %s has unknown version %x (0x1e83b9, 0x1e9179 and 0x2e are currently supported), trying to load anyway!\n", loadmodel->name, pchunk->id, version);
+		if (version != 0x1e83b9 && version != 0x1e9179 && version != 0x2e && version != 0x12f2bc && version != 0x12f2f0)
+			Con_Printf ("%s: chunk %s has unknown version %x (0x1e83b9, 0x1e9179, 0x2e, 0x12f2bc, 0x12f2f0 are currently supported), trying to load anyway!\n", loadmodel->name, pchunk->id, version);
 		if (!strcmp(pchunk->id, "ACTRHEAD"))
 		{
 			// nothing to do
@@ -1968,8 +1968,8 @@ void Mod_PSKMODEL_Load(model_t *mod, void *buffer, void *bufferend)
 		numrecords = LittleLong(pchunk->numrecords);
 		if (developer.integer >= 100)
 			Con_Printf("%s: %s %x: %i * %i = %i\n", animname, pchunk->id, version, recordsize, numrecords, recordsize * numrecords);
-		if (version != 0x1e83b9 && version != 0x1e9179 && version != 0x2e)
-			Con_Printf ("%s: chunk %s has unknown version %x (0x1e83b9, 0x1e9179 and 0x2e are currently supported), trying to load anyway!\n", animname, pchunk->id, version);
+		if (version != 0x1e83b9 && version != 0x1e9179 && version != 0x2e && version != 0x12f2bc && version != 0x12f2f0)
+			Con_Printf ("%s: chunk %s has unknown version %x (0x1e83b9, 0x1e9179, 0x2e, 0x12f2bc, 0x12f2f0 are currently supported), trying to load anyway!\n", animname, pchunk->id, version);
 		if (!strcmp(pchunk->id, "ANIMHEAD"))
 		{
 			// nothing to do
