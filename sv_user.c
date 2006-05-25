6b73170803d39d73032de4a52995aad52d1e6e88
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2006-05-25 23:27:24 +0000

    fixed sv_waterfriction code so it is now used
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@6388 d7cf8633-e32d-0410-b094-e92efae38249

--- sv_user.c
+++ sv_user.c
@@ -273,7 +273,7 @@ void SV_WaterMove (void)
 	speed = VectorLength(host_client->edict->fields.server->velocity);
 	if (speed)
 	{
-		newspeed = speed - sv.frametime * speed * sv_friction.value;
+		newspeed = speed - sv.frametime * speed * sv_waterfriction.value;
 		if (newspeed < 0)
 			newspeed = 0;
 		temp = newspeed/speed;
