cd2a988ed5842fbd909d73d2272064a4c5c964f8
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2006-08-14 22:54:32 +0000

    patch from div0: Send download requirements at an earlier time to cause less message spam with the disconnect-and-reconnect download method
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@6555 d7cf8633-e32d-0410-b094-e92efae38249

--- host_cmd.c
+++ host_cmd.c
@@ -19,7 +19,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "quakedef.h"
-#include "libcurl.h"
 
 int current_skill;
 cvar_t sv_cheats = {0, "sv_cheats", "0", "enables cheat commands in any game, and cheat impulses in dpmod"};
@@ -1527,8 +1526,6 @@ void Host_Begin_f (void)
 		Con_Print("begin is not valid from the console\n");
 		return;
 	}
-
-	Curl_SendRequirements();
 
 	host_client->spawned = true;
 }
