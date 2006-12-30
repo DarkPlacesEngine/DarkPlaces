7bb68dde9f84458a72fa2886a52c5b7efe4ad142
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2006-12-30 16:28:26 +0000

    I don't know how this got committed
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@6672 d7cf8633-e32d-0410-b094-e92efae38249

--- model_brush.c
+++ model_brush.c
@@ -1264,7 +1264,6 @@ static void Mod_Q1BSP_LoadTextures(lump_t *l)
 		tx->skinframerate = 1;
 		tx->currentskinframe = tx->skinframes;
 		tx->skinframes[0].base = r_texture_notexture;
-		tx->backgroundcurrentskinframe = tx->backgroundskinframes;
 		tx->basematerialflags = 0;
 		if (i == loadmodel->num_textures - 1)
 		{
@@ -4397,15 +4396,6 @@ Q3 shader blendfuncs actually used in the game (* = supported by DP)
 					if (!Mod_LoadSkinFrame(&out->skinframes[j], shader->primarylayer->texturename[j], ((shader->surfaceparms & Q3SURFACEPARM_NOMIPMAPS) ? 0 : TEXF_MIPMAP) | TEXF_ALPHA | TEXF_PRECACHE | (shader->textureflags & Q3TEXTUREFLAG_NOPICMIP ? 0 : TEXF_PICMIP) | (shader->primarylayer->clampmap ? TEXF_CLAMP : 0), false, true))
 						Con_Printf("%s: could not load texture \"%s\" (frame %i) for shader \"%s\"\n", loadmodel->name, shader->primarylayer->texturename[j], j, out->name);
 			}
-			if (shader->backgroundlayer && cls.state != ca_dedicated)
-			{
-				int j;
-				out->backgroundnumskinframes = shader->backgroundlayer->numframes;
-				out->backgroundskinframerate = shader->backgroundlayer->framerate;
-				for (j = 0;j < shader->backgroundlayer->numframes;j++)
-					if (!Mod_LoadSkinFrame(&out->backgroundskinframes[j], shader->backgroundlayer->texturename[j], ((shader->surfaceparms & Q3SURFACEPARM_NOMIPMAPS) ? 0 : TEXF_MIPMAP) | TEXF_ALPHA | TEXF_PRECACHE | (shader->textureflags & Q3TEXTUREFLAG_NOPICMIP ? 0 : TEXF_PICMIP) | (shader->backgroundlayer->clampmap ? TEXF_CLAMP : 0), false, true))
-						Con_Printf("%s: could not load texture \"%s\" (frame %i) for shader \"%s\"\n", loadmodel->name, shader->backgroundlayer->texturename[j], j, out->name);
-			}
 		}
 		else
 		{
@@ -4433,7 +4423,6 @@ Q3 shader blendfuncs actually used in the game (* = supported by DP)
 		// init the animation variables
 		out->currentframe = out;
 		out->currentskinframe = &out->skinframes[0];
-		out->backgroundcurrentskinframe = &out->backgroundskinframes[0];
 	}
 	if (c)
 		Con_DPrintf("%s: %i textures missing shaders\n", loadmodel->name, c);
