fdef1fe36bbe55ad8f07800c057edc769902a2b8
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2007-02-18 12:45:26 +0000

    changed fieldoffsets and globaloffsets to be integer array indices instead of byte offsets, this basically means d->ofs is no longer multiplied by 4 in PRVM_ED_FindFieldOffset and PRVM_ED_FindGlobalOffset
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@6852 d7cf8633-e32d-0410-b094-e92efae38249

--- progs.h
+++ progs.h
@@ -137,7 +137,7 @@ typedef struct edict_s
 }
 prvm_edict_t;
 
-#define PRVM_EDICTFIELDVALUE(ed, fieldoffset) (fieldoffset ? (prvm_eval_t *)((unsigned char *)ed->v + fieldoffset) : NULL)
+#define PRVM_EDICTFIELDVALUE(ed, fieldoffset) (fieldoffset ? (prvm_eval_t *)((int *)ed->v + fieldoffset) : NULL)
 
 //============================================================================
 
