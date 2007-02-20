7af96fa3f500f70e1869316d201cdc4d31fbb75f
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2007-02-20 12:32:03 +0000

    fix some crashes related to particles when csqc is not active
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@6879 d7cf8633-e32d-0410-b094-e92efae38249

--- cl_collision.h
+++ cl_collision.h
@@ -11,6 +11,6 @@ model_t *CL_GetModelFromEdict(prvm_edict_t *ed);
 void CL_LinkEdict(prvm_edict_t *ent);
 int CL_GenericHitSuperContentsMask(const prvm_edict_t *edict);
 trace_t CL_Move(const vec3_t start, const vec3_t mins, const vec3_t maxs, const vec3_t end, int type, prvm_edict_t *passedict, int hitsupercontentsmask, qboolean hitnetworkbrushmodels, qboolean hitnetworkplayers, int *hitnetworkentity, qboolean hitcsqcentities);
-#define CL_PointSuperContents(point) (CL_Move((point), vec3_origin, vec3_origin, (point), sv_gameplayfix_swiminbmodels.integer ? MOVE_NOMONSTERS : MOVE_WORLDONLY, NULL, 0, true, false, NULL, true).startsupercontents)
+#define CL_PointSuperContents(point) (CL_Move((point), vec3_origin, vec3_origin, (point), sv_gameplayfix_swiminbmodels.integer ? MOVE_NOMONSTERS : MOVE_WORLDONLY, NULL, 0, true, false, NULL, false).startsupercontents)
 
 #endif
