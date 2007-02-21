b65c15b40f7940791509ef3a5df93ca652a9b7f5
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2007-02-21 11:55:37 +0000

    modified surface renderer batching to batch all lightmapped surfaces with the same texture together, rather than a separate batch for each lightmap texture, this means that the lightmap switching overhead is lower than before, no apparent improvement in benchmarks though
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@6883 d7cf8633-e32d-0410-b094-e92efae38249

--- render.h
+++ render.h
@@ -215,7 +215,7 @@ extern qboolean rsurface_generatedvertex;
 extern const entity_render_t *rsurface_entity;
 extern const model_t *rsurface_model;
 extern texture_t *rsurface_texture;
-extern rtexture_t *rsurface_lightmaptexture;
+extern qboolean rsurface_uselightmaptexture;
 extern rsurfmode_t rsurface_mode;
 
 void RSurf_ActiveEntity(const entity_render_t *ent, qboolean wantnormals, qboolean wanttangents);
