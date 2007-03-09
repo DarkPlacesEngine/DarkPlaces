8c2cc46c445ce95569c799684fca33bb5d1593ab
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2007-03-09 02:39:07 +0000

    removed master 12.166.196.192 (run by Venim) because it hasn't been responding for a long time
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@6947 d7cf8633-e32d-0410-b094-e92efae38249

--- netconn.c
+++ netconn.c
@@ -38,8 +38,7 @@ static cvar_t sv_masters [] =
 	{CVAR_SAVE, "sv_master4", "", "user-chosen master server 4"},
 	{0, "sv_masterextra1", "ghdigital.com", "default master server 1 (admin: LordHavoc)"}, // admin: LordHavoc
 	{0, "sv_masterextra2", "dpmaster.deathmask.net", "default master server 2 (admin: Willis)"}, // admin: Willis
-	{0, "sv_masterextra3", "12.166.196.192", "default master server 3 (admin: Venim)"}, // admin: Venim
-	{0, "sv_masterextra4", "excalibur.nvg.ntnu.no", "default master server 4 (admin: tChr)"}, // admin: tChr
+	{0, "sv_masterextra3", "excalibur.nvg.ntnu.no", "default master server 3 (admin: tChr)"}, // admin: tChr
 	{0, NULL, NULL, NULL}
 };
 
