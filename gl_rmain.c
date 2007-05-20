c385fc96c504e55bbaa18bc91103941cdcc0eb37
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2007-05-20 22:26:48 +0000

    made the effect of the r_shadow_shadow_polygonoffset cvar reverse when r_shadow_frontsidecasting is off, so that only one setting needs changing to toggle between the two modes
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@7324 d7cf8633-e32d-0410-b094-e92efae38249

--- gl_rmain.c
+++ gl_rmain.c
@@ -2455,8 +2455,8 @@ void R_UpdateVariables(void)
 
 	r_refdef.polygonfactor = 0;
 	r_refdef.polygonoffset = 0;
-	r_refdef.shadowpolygonfactor = r_refdef.polygonfactor + r_shadow_shadow_polygonfactor.value;
-	r_refdef.shadowpolygonoffset = r_refdef.polygonoffset + r_shadow_shadow_polygonoffset.value;
+	r_refdef.shadowpolygonfactor = r_refdef.polygonfactor + r_shadow_shadow_polygonfactor.value * (r_shadow_frontsidecasting.integer ? 1 : -1);
+	r_refdef.shadowpolygonoffset = r_refdef.polygonoffset + r_shadow_shadow_polygonoffset.value * (r_shadow_frontsidecasting.integer ? 1 : -1);
 
 	r_refdef.rtworld = r_shadow_realtime_world.integer;
 	r_refdef.rtworldshadows = r_shadow_realtime_world_shadows.integer && gl_stencil;
