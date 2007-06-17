6e4796b236d8ef0d4c4127689fbaaf25815cbe0c
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2007-06-17 07:32:52 +0000

    added view_angles field in csqc, no longer sets pmove_org or pmove_vel
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@7441 d7cf8633-e32d-0410-b094-e92efae38249

--- progsvm.h
+++ progsvm.h
@@ -221,6 +221,7 @@ typedef struct prvm_prog_globaloffsets_s
 	int v_forward; // ssqc / csqc
 	int v_right; // ssqc / csqc
 	int v_up; // ssqc / csqc
+	int view_angles; // csqc
 	int trace_allsolid; // ssqc / csqc
 	int trace_startsolid; // ssqc / csqc
 	int trace_fraction; // ssqc / csqc
