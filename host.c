3138e5834ce8b442b7af91eb8438968d3874558b
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2007-09-02 13:57:58 +0000

    doubled cl_maxidlefps, this cures sound stuttering when the window is inactive
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@7552 d7cf8633-e32d-0410-b094-e92efae38249

--- host.c
+++ host.c
@@ -58,7 +58,7 @@ cvar_t host_framerate = {0, "host_framerate","0", "locks frame timing to this va
 cvar_t host_speeds = {0, "host_speeds","0", "reports how much time is used in server/graphics/sound"};
 // LordHavoc: framerate upper cap
 cvar_t cl_maxfps = {CVAR_SAVE, "cl_maxfps", "1000", "maximum fps cap, if game is running faster than this it will wait before running another frame (useful to make cpu time available to other programs)"};
-cvar_t cl_maxidlefps = {CVAR_SAVE, "cl_maxidlefps", "10", "maximum fps cap when the game is not the active window (makes cpu time available to other programs"};
+cvar_t cl_maxidlefps = {CVAR_SAVE, "cl_maxidlefps", "20", "maximum fps cap when the game is not the active window (makes cpu time available to other programs"};
 
 cvar_t developer = {0, "developer","0", "prints additional debugging messages and information (recommended for modders and level designers)"};
 cvar_t developer_entityparsing = {0, "developer_entityparsing", "0", "prints detailed network entities information each time a packet is received"};
