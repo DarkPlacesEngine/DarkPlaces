4f2641c2845eb4c4220fcec3c19ee8833a548fbd
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2007-11-09 13:25:10 +0000

    (Nexuiz) show people who are still downloading as -666 (like spectators) in scoreboard. Should get replaced by some way to completely hide them from scoreboard, but still show them in status, ping etc.
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@7667 d7cf8633-e32d-0410-b094-e92efae38249

--- sv_main.c
+++ sv_main.c
@@ -1932,6 +1932,9 @@ static void SV_UpdateToReliableMessages (void)
 
 		// frags
 		host_client->frags = (int)host_client->edict->fields.server->frags;
+		if(gamemode == GAME_NEXUIZ)
+			if(!host_client->spawned)
+				host_client->frags = -666;
 		if (host_client->old_frags != host_client->frags)
 		{
 			host_client->old_frags = host_client->frags;
