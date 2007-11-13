358412c2f02bf2df4a98519387d1f2ed3167facf
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2007-11-13 20:25:53 +0000

    SDL icon support by Blub, messed up by me ;)
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@7697 d7cf8633-e32d-0410-b094-e92efae38249

--- vid.h
+++ vid.h
@@ -22,6 +22,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef VID_H
 #define VID_H
 
+#define ENGINE_ICON ( (gamemode == GAME_NEXUIZ) ? nexuiz_xpm : darkplaces_xpm )
+
 extern int cl_available;
 
 typedef struct viddef_s
