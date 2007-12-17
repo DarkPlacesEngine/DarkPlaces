834d524b6cd5f96d8c70eb241d9cb03c622e0dd2
res <res@d7cf8633-e32d-0410-b094-e92efae38249>
2007-12-17 03:58:51 +0000

    use strlcpy() instead of strcpy()
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@7810 d7cf8633-e32d-0410-b094-e92efae38249

--- zone.c
+++ zone.c
@@ -561,9 +561,10 @@ void MemStats_f(void)
 char* Mem_strdup (mempool_t *pool, const char* s)
 {
 	char* p;
+	size_t sz = strlen (s) + 1;
 	if (s == NULL) return NULL;
-	p = (char*)Mem_Alloc (pool, strlen (s) + 1);
-	strcpy (p, s);
+	p = (char*)Mem_Alloc (pool, sz);
+	strlcpy (p, s, sz);
 	return p;
 }
 
