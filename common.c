7775d7f413d36055857539ba8e1862f8ea942d82
res <res@d7cf8633-e32d-0410-b094-e92efae38249>
2008-01-24 01:56:18 +0000

    edu2p game: change 2nd game dir to 'edu2'
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@8003 d7cf8633-e32d-0410-b094-e92efae38249

--- common.c
+++ common.c
@@ -1485,7 +1485,7 @@ static const gamemode_info_t gamemode_info [] =
 { "contagiontheory",		"-contagiontheory",	"Contagion Theory",			"ctdata",	NULL, 			"ct",			"contagiontheory" },
 // GAME_EDU2P
 // COMMANDLINEOPTION: Game: -edu2p runs the game Edu2 prototype
-{ "edu2p", "-edu2p", "EDU2 Prototype", "id1", "data", "edu2_p", "edu2prototype" },
+{ "edu2p", "-edu2p", "EDU2 Prototype", "id1", "edu2", "edu2_p", "edu2prototype" },
 };
 
 void COM_InitGameType (void)
