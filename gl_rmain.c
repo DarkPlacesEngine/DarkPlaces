463e763b9dea3d318821a7a588fe60a7b9f91d21
black <black@d7cf8633-e32d-0410-b094-e92efae38249>
2008-02-27 22:11:08 +0000

    Don't try to render something when height or width are zero.
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@8153 d7cf8633-e32d-0410-b094-e92efae38249

--- gl_rmain.c
+++ gl_rmain.c
@@ -3684,7 +3684,7 @@ R_RenderView
 */
 void R_RenderView(void)
 {
-	if (!r_refdef.scene.entities/* || !r_refdef.scene.worldmodel*/)
+	if (!r_refdef.scene.entities || r_refdef.view.width * r_refdef.view.height == 0/* || !r_refdef.scene.worldmodel*/)
 		return; //Host_Error ("R_RenderView: NULL worldmodel");
 
 	r_refdef.view.colorscale = r_hdr_scenebrightness.value;
