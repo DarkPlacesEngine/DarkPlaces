7b85996a847b1dd078c99e1938eeb89d56848c2b
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2008-02-29 10:37:26 +0000

    use a better cast on a parameter in a ToAscii call, might fix a warning, probably not
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@8162 d7cf8633-e32d-0410-b094-e92efae38249

--- vid_wgl.c
+++ vid_wgl.c
@@ -527,7 +527,7 @@ LONG WINAPI MainWndProc (HWND hWnd, UINT uMsg, WPARAM  wParam, LPARAM lParam)
 			GetKeyboardState (state);
 			// alt/ctrl/shift tend to produce funky ToAscii values,
 			// and if it's not a single character we don't know care about it
-			charlength = ToAscii (wParam, lParam >> 16, state, (unsigned short *)asciichar, 0);
+			charlength = ToAscii (wParam, lParam >> 16, state, (LPWORD)asciichar, 0);
 			if (vkey == K_ALT || vkey == K_CTRL || vkey == K_SHIFT || charlength == 0)
 				asciichar[0] = 0;
 			else if( charlength == 2 ) {
