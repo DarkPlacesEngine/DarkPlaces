41eb36f0217fd3898ef6ae15c9f8f862d627f1b7
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2008-04-13 14:17:10 +0000

    fix "wrong ping times" issue once and for all (I don't see another fix that wouldn't at the same time re-enable a speedhack)
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@8255 d7cf8633-e32d-0410-b094-e92efae38249

--- client.h
+++ client.h
@@ -355,8 +355,9 @@ typedef struct usercmd_s
 	vec_t	cursor_fraction;
 	int		cursor_entitynumber;
 
-	double time;
-	double receivetime;
+	double time; // time the move is executed for (cl_movement: clienttime, non-cl_movement: receivetime)
+	double receivetime; // time the move was received at
+	double clienttime; // time to which server state the move corresponds to
 	int msec; // for predicted moves
 	int buttons;
 	int impulse;
