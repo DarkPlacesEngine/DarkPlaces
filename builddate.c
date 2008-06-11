1346a06ebda6590749c578328718b7b490e2aa03
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2008-06-11 11:20:33 +0000

    also put svn revision (svnversion(1)) and build type into the version string
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@8353 d7cf8633-e32d-0410-b094-e92efae38249

--- builddate.c
+++ builddate.c
@@ -1,3 +1,11 @@
+#define STRINGIFY2(arg) #arg
+#define STRINGIFY(arg) STRINGIFY2(arg)
 
-char *buildstring = __TIME__ " " __DATE__;
-
+char *buildstring = __TIME__ " " __DATE__
+#ifdef SVNREVISION
+" " STRINGIFY(SVNREVISION)
+#endif
+#ifdef BUILDTYPE
+" " STRINGIFY(BUILDTYPE)
+#endif
+;
