cf5b8c6c279111a4dec859a466b468ed6eb69eb6
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2008-07-23 09:39:37 +0000

    turn rev8420 "enable transparency on tga skins on mdl/md2/md3 when shader is not found" into what it actually WANTED to be (only enable the transparency if the TGA actually is transparent)
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@8423 d7cf8633-e32d-0410-b094-e92efae38249

--- render.h
+++ render.h
@@ -140,6 +140,7 @@ void R_SkinFrame_Purge(void);
 skinframe_t *R_SkinFrame_FindNextByName( skinframe_t *last, const char *name );
 skinframe_t *R_SkinFrame_Find(const char *name, int textureflags, int comparewidth, int compareheight, int comparecrc, qboolean add);
 skinframe_t *R_SkinFrame_LoadExternal(const char *name, int textureflags, qboolean complain);
+skinframe_t *R_SkinFrame_LoadExternal_CheckAlpha(const char *name, int textureflags, qboolean complain, qboolean *has_alpha);
 skinframe_t *R_SkinFrame_LoadInternalBGRA(const char *name, int textureflags, const unsigned char *skindata, int width, int height);
 skinframe_t *R_SkinFrame_LoadInternalQuake(const char *name, int textureflags, int loadpantsandshirt, int loadglowtexture, const unsigned char *skindata, int width, int height);
 skinframe_t *R_SkinFrame_LoadMissing(void);
