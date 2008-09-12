815455055dcc371ce219adb3f4b6f7f162b7cf9e
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2008-09-12 08:14:24 +0000

    add extension DP_QC_URI_GET (downloads HTTP/whatever URLs to QC strings using a callback)
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@8502 d7cf8633-e32d-0410-b094-e92efae38249

--- prvm_edict.c
+++ prvm_edict.c
@@ -1482,6 +1482,7 @@ void PRVM_FindOffsets(void)
 	prog->funcoffsets.SV_OnEntityNoSpawnFunction      = PRVM_ED_FindFunctionOffset("SV_OnEntityNoSpawnFunction");
 	prog->funcoffsets.GameCommand                     = PRVM_ED_FindFunctionOffset("GameCommand");
 	prog->funcoffsets.SV_Shutdown                     = PRVM_ED_FindFunctionOffset("SV_Shutdown");
+	prog->funcoffsets.URI_Get_Callback                = PRVM_ED_FindFunctionOffset("URI_Get_Callback");
 	prog->globaloffsets.SV_InitCmd                    = PRVM_ED_FindGlobalOffset("SV_InitCmd");
 	prog->globaloffsets.self                          = PRVM_ED_FindGlobalOffset("self");
 	prog->globaloffsets.time                          = PRVM_ED_FindGlobalOffset("time");
