153880233d4e1fcbee9b0c49d8f55bb05fcd8230
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2009-01-28 13:55:38 +0000

    fix a type
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@8685 d7cf8633-e32d-0410-b094-e92efae38249

--- console.c
+++ console.c
@@ -124,7 +124,7 @@ cvar_t log_file = {0, "log_file","", "filename to log messages to"};
 cvar_t log_dest_udp = {0, "log_dest_udp","", "UDP address to log messages to (in QW rcon compatible format); multiple destinations can be separated by spaces; DO NOT SPECIFY DNS NAMES HERE"};
 char log_dest_buffer[1400]; // UDP packet
 size_t log_dest_buffer_pos;
-qboolean log_dest_buffer_appending;
+unsigned int log_dest_buffer_appending;
 char crt_log_file [MAX_OSPATH] = "";
 qfile_t* logfile = NULL;
 
