f8b26a08b95e147de401f60589a6465c22fc7461
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2009-02-18 20:06:05 +0000

    now, for mingw, use the uint32_t names instead of the u_int32_t ones
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@8730 d7cf8633-e32d-0410-b094-e92efae38249

--- cap_ogg.c
+++ cap_ogg.c
@@ -24,9 +24,9 @@ typedef unsigned __int32 ogg_uint32_t;
 typedef __int64 ogg_int64_t;
 #else
 typedef int16_t ogg_int16_t;
-typedef u_int16_t ogg_uint16_t;
+typedef uint16_t ogg_uint16_t;
 typedef int32_t ogg_int32_t;
-typedef u_int32_t ogg_uint32_t;
+typedef uint32_t ogg_uint32_t;
 typedef int64_t ogg_int64_t;
 #endif
 
