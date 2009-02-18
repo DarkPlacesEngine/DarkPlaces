c4e92b9982882076e7b35e55fb43bd1a0dbd340d
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2009-02-18 14:05:10 +0000

    capturevideo refactoring, making AVI also "just a module" for it
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@8725 d7cf8633-e32d-0410-b094-e92efae38249

--- cap_ogg.h
+++ cap_ogg.h
@@ -1,8 +1,4 @@
-#include "snd_main.h"
 void SCR_CaptureVideo_Ogg_Init();
 qboolean SCR_CaptureVideo_Ogg_Available();
-void SCR_CaptureVideo_Ogg_Begin();
-void SCR_CaptureVideo_Ogg_EndVideo();
-void SCR_CaptureVideo_Ogg_VideoFrame();
-void SCR_CaptureVideo_Ogg_SoundFrame(const portable_sampleframe_t *paintbuffer, size_t length);
+void SCR_CaptureVideo_Ogg_BeginVideo();
 void SCR_CaptureVideo_Ogg_CloseDLL();
