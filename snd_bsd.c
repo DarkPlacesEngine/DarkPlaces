65f6c04779aa58c69929e0d22967bf152dac17c1
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2009-09-25 12:59:45 +0000

    snd_alsa: MIDI input support! MIDI events get mapped to MIDINOTE<n> events (n = 0 to 127)
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@9232 d7cf8633-e32d-0410-b094-e92efae38249

--- snd_bsd.c
+++ snd_bsd.c
@@ -230,3 +230,15 @@ void SndSys_UnlockRenderBuffer (void)
 {
 	// Nothing to do
 }
+
+/*
+====================
+SndSys_SendKeyEvents
+
+Send keyboard events originating from the sound system (e.g. MIDI)
+====================
+*/
+void SndSys_SendKeyEvents(void)
+{
+	// not supported
+}
--- snd_sdl.c
+++ snd_sdl.c
@@ -249,3 +249,15 @@ void SndSys_UnlockRenderBuffer (void)
 {
 	SDL_UnlockAudio();
 }
+
+/*
+====================
+SndSys_SendKeyEvents
+
+Send keyboard events originating from the sound system (e.g. MIDI)
+====================
+*/
+void SndSys_SendKeyEvents(void)
+{
+	// not supported
+}
