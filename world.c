40ede0998143855ae68b3399d8c5ed4313f79f47
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2009-10-22 08:58:55 +0000

    due to huge slowdowns even in non-ODE using games (probably from body syncing), turn off ODE_DYNAMIC by default again until this is solved (e.g. by performing no ODE stuff if there is no single MOVETYPE_PHYSICS ent)
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@9381 d7cf8633-e32d-0410-b094-e92efae38249

--- world.c
+++ world.c
@@ -314,9 +314,9 @@ void World_LinkEdict(world_t *world, prvm_edict_t *ent, const vec3_t mins, const
 // physics engine support
 //============================================================================
 
-#ifndef ODE_STATIC
-#define ODE_DYNAMIC 1
-#endif
+//#ifndef ODE_STATIC
+//#define ODE_DYNAMIC 1
+//#endif
 
 #if defined(ODE_STATIC) || defined(ODE_DYNAMIC)
 #define USEODE 1
