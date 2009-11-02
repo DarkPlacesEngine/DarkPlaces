760a7d9bb6b99988bca9d5419f748004d82e7b58
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2009-11-02 00:43:04 +0000

    fix compatibility with old windows versions when USE_WSPIAPI_H is defined
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@9409 d7cf8633-e32d-0410-b094-e92efae38249

--- lhnet.c
+++ lhnet.c
@@ -3,10 +3,13 @@
 
 #ifdef WIN32
 // Windows XP or higher is required for getaddrinfo, but the inclusion of wspiapi provides fallbacks for older versions
-#define _WIN32_WINNT 0x0501
-#include <winsock2.h>
-#include <ws2tcpip.h>
-//#include <wspiapi.h>
+# define _WIN32_WINNT 0x0501
+# include <winsock2.h>
+# ifdef USE_WSPIAPI_H
+#  include <wspiapi.h>
+# else
+#  include <ws2tcpip.h>
+# endif
 #endif
 
 #ifndef STANDALONETEST
