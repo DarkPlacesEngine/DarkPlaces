7173807acd2f26187cf1263a35a4f45a7f92fe63
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2009-12-07 14:18:15 +0000

    fix a warning (gl_driver is not a pointer)
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@9548 d7cf8633-e32d-0410-b094-e92efae38249

--- vid_shared.c
+++ vid_shared.c
@@ -983,7 +983,7 @@ void VID_CheckExtensions(void)
 	Cvar_SetQuick(&gl_info_renderer, gl_renderer);
 	Cvar_SetQuick(&gl_info_version, gl_version);
 	Cvar_SetQuick(&gl_info_platform, gl_platform ? gl_platform : "");
-	Cvar_SetQuick(&gl_info_driver, gl_driver ? gl_driver : "");
+	Cvar_SetQuick(&gl_info_driver, gl_driver);
 }
 
 void Force_CenterView_f (void)
