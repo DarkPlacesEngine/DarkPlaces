5f5145103b0f3c344d3fdfcbb1e94b3dd6953513
blub <blub@d7cf8633-e32d-0410-b094-e92efae38249>
2009-12-23 11:46:09 +0000

    this should work without strlen as well
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@9644 d7cf8633-e32d-0410-b094-e92efae38249

--- common.c
+++ common.c
@@ -740,9 +740,11 @@ int COM_Wordwrap(const char *string, size_t length, float continuationWidth, flo
 					}
 				}
 				out_inner:
+				/*
 				wordChars = strlen(cursor);
 				if (wordChars > wordLen)
-					wordChars = wordLen;
+				*/
+				wordChars = wordLen;
 				spaceUsedForWord = wordWidth(passthroughCW, cursor, &wordChars, maxWidth - continuationWidth); // this may have reduced wordLen when it won't fit - but this is GOOD. TODO fix words that do fit in a non-continuation line
 				if(wordChars < 1)
 				{
