9be9ae4fc976fb1118774d81fd5033d2c5c33bdb
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2010-01-18 10:11:10 +0000

    fix a warning
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@9838 d7cf8633-e32d-0410-b094-e92efae38249
    ::stable-branch::merge=a34878eed8675af1813ed36c696733d381955cf4

--- keys.c
+++ keys.c
@@ -712,7 +712,7 @@ Key_Console (int key, int unicode)
 		else if(keydown[K_SHIFT]) // move cursor to the previous character ignoring colors
 		{
 			int		pos;
-			size_t          inchar;
+			size_t          inchar = 0;
 			pos = u8_prevbyte(key_line, key_linepos);
 			while (pos)
 				if(pos-1 > 0 && key_line[pos-1] == STRING_COLOR_TAG && isdigit(key_line[pos]))
