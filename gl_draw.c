9d6c1a719369bf5bb6673c885a79b33a000fa758
vortex <vortex@d7cf8633-e32d-0410-b094-e92efae38249>
2010-05-23 12:11:33 +0000

    try fix a warning
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@10216 d7cf8633-e32d-0410-b094-e92efae38249
    ::stable-branch::merge=aacc356f7442208c775971b3abb71cea0b749b2d

--- gl_draw.c
+++ gl_draw.c
@@ -870,7 +870,7 @@ static void gl_draw_start(void)
 	// load default font textures
 	for(i = 0; i < dp_fonts.maxsize; ++i)
 		if (dp_fonts.f[i].title[0])
-			LoadFont(false, va("gfx/font_%s", &dp_fonts.f[i].title), &dp_fonts.f[i], 1, 0);
+			LoadFont(false, va("gfx/font_%s", dp_fonts.f[i].title), &dp_fonts.f[i], 1, 0);
 
 	// draw the loading screen so people have something to see in the newly opened window
 	SCR_UpdateLoadingScreen(true);
