9d0aba9a9b138e46170bdc6a7a5581f7b00e4892
vortex <vortex@d7cf8633-e32d-0410-b094-e92efae38249>
2010-05-24 18:20:25 +0000

    fix a bug with font scale/voffset not surviving r_restart by moving this settings to dp_font->settings. Added "scale" and "voffset" custom switches to loadfont console command.
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@10218 d7cf8633-e32d-0410-b094-e92efae38249
    ::stable-branch::merge=e9e658105ffd3f155b6f874fd79cbef340629748

--- draw.h
+++ draw.h
@@ -92,6 +92,7 @@ DRAWFLAG_MIPMAP = 0x100 // ONLY R_BeginPolygon()
 
 typedef struct ft2_settings_s
 {
+	float scale, voffset;
 	// cvar parameters (only read on loadfont command)
 	int antialias, hinting;
 	float outline, blur, shadowx, shadowy, shadowz;
@@ -104,8 +105,6 @@ typedef struct dp_font_s
 	rtexture_t *tex;
 	float width_of[256]; // width_of[0] == max width of any char; 1.0f is base width (1/16 of texture width); therefore, all widths have to be <= 1 (does not include scale)
 	float maxwidth; // precalculated max width of the font (includes scale)
-	float scale; // scales the font (without changing line height!)
-	float voffset; // offsets the font up/down (without changing line position)
 	char texpath[MAX_QPATH];
 	char title[MAX_QPATH];
 
