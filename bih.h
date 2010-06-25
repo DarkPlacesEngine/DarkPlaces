3af0b7fb7378c916612c11f76e5880164497a0d1
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2010-06-25 11:53:41 +0000

    added BIH_GetTriangleListForBox function
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@10250 d7cf8633-e32d-0410-b094-e92efae38249

--- bih.h
+++ bih.h
@@ -80,4 +80,6 @@ bih_t;
 
 int BIH_Build(bih_t *bih, int numleafs, bih_leaf_t *leafs, int maxnodes, bih_node_t *nodes, int *temp_leafsort, int *temp_leafsortscratch);
 
+int BIH_GetTriangleListForBox(const bih_t *bih, int maxtriangles, int *trianglelist, const float *mins, const float *maxs);
+
 #endif
