1c76312b52aad3565537a423344fa4d9f543e6c3
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2010-10-17 14:15:06 +0000

    fix two types to hopefulyl compile on MSVC
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@10543 d7cf8633-e32d-0410-b094-e92efae38249

--- netconn.c
+++ netconn.c
@@ -2285,7 +2285,7 @@ static qboolean NetConn_BuildStatusResponse(const char* challenge, char* out_msg
 			char *p;
 			const char *q;
 			p = qcstatus;
-			for(q = str; *q && p - qcstatus < (ssize_t)(sizeof(qcstatus)) - 1; ++q)
+			for(q = str; *q && p - qcstatus < (ptrdiff_t)(sizeof(qcstatus)) - 1; ++q)
 				if(*q != '\\' && *q != '\n')
 					*p++ = *q;
 			*p = 0;
