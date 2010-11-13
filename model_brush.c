6acdb81fe2b7489fb2fa6c7c07b73e4a9ba374d0
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2010-11-13 14:05:04 +0000

    show more detail in "has no triangles" and "has no vertexes" warning
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@10595 d7cf8633-e32d-0410-b094-e92efae38249

--- model_brush.c
+++ model_brush.c
@@ -5319,9 +5319,16 @@ static void Mod_Q3BSP_LoadFaces(lump_t *l)
 		if(out->num_vertices && out->num_triangles)
 			continue;
 		if(out->num_vertices == 0)
-			Con_Printf("Mod_Q3BSP_LoadFaces: surface %d has no vertices, ignoring\n", i);
-		if(out->num_triangles == 0)
-			Con_Printf("Mod_Q3BSP_LoadFaces: surface %d has no triangles, ignoring\n", i);
+		{
+			Con_Printf("Mod_Q3BSP_LoadFaces: surface %d (texture %s) has no vertices, ignoring\n", i, out->texture->name);
+			if(out->num_triangles == 0)
+				Con_Printf("Mod_Q3BSP_LoadFaces: surface %d (texture %s) has no triangles, ignoring\n", i, out->texture->name);
+		}
+		else if(out->num_triangles == 0)
+			Con_Printf("Mod_Q3BSP_LoadFaces: surface %d (texture %s, near %f %f %f) has no triangles, ignoring\n", i, out->texture->name,
+					(loadmodel->surfmesh.data_vertex3f + 3 * out->num_firstvertex)[0 * 3 + 0],
+					(loadmodel->surfmesh.data_vertex3f + 3 * out->num_firstvertex)[1 * 3 + 0],
+					(loadmodel->surfmesh.data_vertex3f + 3 * out->num_firstvertex)[2 * 3 + 0]);
 	}
 
 	// for per pixel lighting
