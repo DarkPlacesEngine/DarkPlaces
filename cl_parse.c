7e3d1e3957d8b5f7c4586ac3a99af16f63e16c31
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2010-11-18 02:45:31 +0000

    don't spam warnings about bogus frame numbers in states, because FvF does this all the time, developer_extra 1 will reenable these
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@10605 d7cf8633-e32d-0410-b094-e92efae38249

--- cl_parse.c
+++ cl_parse.c
@@ -1909,11 +1909,14 @@ void CL_ValidateState(entity_state_t *s)
 	if (!(s->flags & RENDER_COLORMAPPED) && s->colormap > cl.maxclients)
 		Con_DPrintf("CL_ValidateState: colormap (%i) > cl.maxclients (%i)\n", s->colormap, cl.maxclients);
 
-	model = CL_GetModelByIndex(s->modelindex);
-	if (model && model->type && s->frame >= model->numframes)
-		Con_DPrintf("CL_ValidateState: no such frame %i in \"%s\" (which has %i frames)\n", s->frame, model->name, model->numframes);
-	if (model && model->type && s->skin > 0 && s->skin >= model->numskins && !(s->lightpflags & PFLAGS_FULLDYNAMIC))
-		Con_DPrintf("CL_ValidateState: no such skin %i in \"%s\" (which has %i skins)\n", s->skin, model->name, model->numskins);
+	if (developer_extra.integer)
+	{
+		model = CL_GetModelByIndex(s->modelindex);
+		if (model && model->type && s->frame >= model->numframes)
+			Con_DPrintf("CL_ValidateState: no such frame %i in \"%s\" (which has %i frames)\n", s->frame, model->name, model->numframes);
+		if (model && model->type && s->skin > 0 && s->skin >= model->numskins && !(s->lightpflags & PFLAGS_FULLDYNAMIC))
+			Con_DPrintf("CL_ValidateState: no such skin %i in \"%s\" (which has %i skins)\n", s->skin, model->name, model->numskins);
+	}
 }
 
 void CL_MoveLerpEntityStates(entity_t *ent)
