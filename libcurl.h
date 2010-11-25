d63934a766353f725bec1e08ef0dafa583be0fd5
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2010-11-25 18:59:39 +0000

    support passing extra headers in POST requests; but this is not used yet
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@10629 d7cf8633-e32d-0410-b094-e92efae38249

--- libcurl.h
+++ libcurl.h
@@ -14,7 +14,7 @@ qboolean Curl_Running(void);
 qboolean Curl_Begin_ToFile(const char *URL, double maxspeed, const char *name, qboolean ispak, qboolean forthismap);
 
 qboolean Curl_Begin_ToMemory(const char *URL, double maxspeed, unsigned char *buf, size_t bufsize, curl_callback_t callback, void *cbdata);
-qboolean Curl_Begin_ToMemory_POST(const char *URL, double maxspeed, const char *post_content_type, const unsigned char *postbuf, size_t postbufsize, unsigned char *buf, size_t bufsize, curl_callback_t callback, void *cbdata);
+qboolean Curl_Begin_ToMemory_POST(const char *URL, const char *extraheaders, double maxspeed, const char *post_content_type, const unsigned char *postbuf, size_t postbufsize, unsigned char *buf, size_t bufsize, curl_callback_t callback, void *cbdata);
 	// NOTE: if these return false, the callback will NOT get called, so free your buffer then!
 void Curl_Cancel_ToMemory(curl_callback_t callback, void *cbdata);
 	// removes all downloads with the given callback and cbdata (this does NOT call the callbacks!)
