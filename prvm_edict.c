aac18a362158962dcf3cc68b78fc71911d916c0d
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2011-02-15 06:46:02 +0000

    because Quake is insane, do NOT save vars matching _? even if not _x, _y, _z
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@10846 d7cf8633-e32d-0410-b094-e92efae38249

--- prvm_edict.c
+++ prvm_edict.c
@@ -732,8 +732,9 @@ void PRVM_ED_Write (qfile_t *f, prvm_edict_t *ed)
 		if(developer_entityparsing.integer)
 			Con_Printf("PRVM_ED_Write: at entity %d field %s\n", PRVM_NUM_FOR_EDICT(ed), name);
 
-		if(strlen(name) > 1 && name[strlen(name)-2] == '_' && (name[strlen(name)-1] == 'x' || name[strlen(name)-1] == 'y' || name[strlen(name)-1] == 'z')
-			continue;	// skip _x, _y, _z vars
+		//if(strlen(name) > 1 && name[strlen(name)-2] == '_' && (name[strlen(name)-1] == 'x' || name[strlen(name)-1] == 'y' || name[strlen(name)-1] == 'z')
+		if(strlen(name) > 1 && name[strlen(name)-2] == '_')
+			continue;	// skip _x, _y, _z vars, and ALSO other _? vars as some mods expect them to be never saved (TODO: a gameplayfix for using the "more precise" condition above?)
 
 		v = (int *)(ed->fields.vp + d->ofs);
 
