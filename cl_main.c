b27d405993c999fa4e301db6cb2b26933a44641d
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2011-06-26 22:37:13 +0000

    implemented DP_ENT_TRAILEFFECTNUM extension, which allows self.traileffectnum = particleeffectnum("mycustomeffect") on server entities, rather than requiring csqc for trailparticles()
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@11220 d7cf8633-e32d-0410-b094-e92efae38249
    ::stable-branch::merge=5070a71b37b3dde085b45ff86fd36c5bb274a039

--- cl_main.c
+++ cl_main.c
@@ -1261,6 +1261,8 @@ void CL_UpdateNetworkEntityTrail(entity_t *e)
 	// do trails
 	if (e->render.flags & RENDER_GLOWTRAIL)
 		trailtype = EFFECT_TR_GLOWTRAIL;
+	if (e->state_current.traileffectnum)
+		trailtype = e->state_current.traileffectnum;
 	// check if a trail is allowed (it is not after a teleport for example)
 	if (trailtype && e->persistent.trail_allowed)
 	{
@@ -1558,6 +1560,8 @@ void CL_LinkNetworkEntity(entity_t *e)
 	// do trail light
 	if (e->render.flags & RENDER_GLOWTRAIL)
 		trailtype = EFFECT_TR_GLOWTRAIL;
+	if (e->state_current.traileffectnum)
+		trailtype = e->state_current.traileffectnum;
 	if (trailtype)
 		CL_ParticleTrail(trailtype, 1, origin, origin, vec3_origin, vec3_origin, NULL, e->state_current.glowcolor, true, false, NULL, NULL);
 
