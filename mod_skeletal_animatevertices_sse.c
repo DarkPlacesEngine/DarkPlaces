0a960a700fce40d45c55d87b2d81c2468727daf3
eihrul <eihrul@d7cf8633-e32d-0410-b094-e92efae38249>
2011-09-30 14:05:06 +0000

    better fix for skeletal no-bones case
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@11377 d7cf8633-e32d-0410-b094-e92efae38249
    ::stable-branch::merge=d67118fd26deb0852127704246098681e0ac5399

--- mod_skeletal_animatevertices_sse.c
+++ mod_skeletal_animatevertices_sse.c
@@ -20,18 +20,6 @@ void Mod_Skeletal_AnimateVertices_SSE(const dp_model_t * RESTRICT model, const f
 	const blendweights_t * RESTRICT weights;
 	int num_vertices_minus_one;
 
-	if (!model->surfmesh.num_vertices)
-		return;
-
-	if (!model->num_bones)
-	{
-		if (vertex3f) memcpy(vertex3f, model->surfmesh.data_vertex3f, model->surfmesh.num_vertices*sizeof(float[3]));
-		if (normal3f) memcpy(normal3f, model->surfmesh.data_normal3f, model->surfmesh.num_vertices*sizeof(float[3]));
-		if (svector3f) memcpy(svector3f, model->surfmesh.data_svector3f, model->surfmesh.num_vertices*sizeof(float[3]));
-		if (tvector3f) memcpy(tvector3f, model->surfmesh.data_tvector3f, model->surfmesh.num_vertices*sizeof(float[3]));
-		return;
-	}
-
 	num_vertices_minus_one = model->surfmesh.num_vertices - 1;
 
 	//unsigned long long ts = rdtsc();
