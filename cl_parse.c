c88063697a4bbe0021c5b24938579136ba35e277
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2011-10-01 14:08:36 +0000

    CSQC_Event_Sound: pass pitch shift info; actually apply received pitch shift data
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@11383 d7cf8633-e32d-0410-b094-e92efae38249

--- cl_parse.c
+++ cl_parse.c
@@ -195,7 +195,7 @@ void QW_CL_StartUpload(unsigned char *data, int size);
 //static qboolean QW_CL_IsUploading(void);
 static void QW_CL_StopUpload(void);
 void CL_VM_UpdateIntermissionState(int intermission);
-qboolean CL_VM_Event_Sound(int sound_num, float volume, int channel, float attenuation, int ent, vec3_t pos);
+qboolean CL_VM_Event_Sound(int sound_num, float volume, int channel, float attenuation, int ent, vec3_t pos, float speed);
 
 /*
 ==================
@@ -289,8 +289,8 @@ void CL_ParseStartSoundPacket(int largesoundindex)
 	if (ent >= cl.max_entities)
 		CL_ExpandEntities(ent);
 
-	if( !CL_VM_Event_Sound(sound_num, volume / 255.0f, channel, attenuation, ent, pos) )
-		S_StartSound (ent, channel, cl.sound_precache[sound_num], pos, volume/255.0f, attenuation);
+	if( !CL_VM_Event_Sound(sound_num, volume / 255.0f, channel, attenuation, ent, pos, speed) )
+		S_StartSound_StartPosition_Flags (ent, channel, cl.sound_precache[sound_num], pos, volume/255.0f, attenuation, 0, CHANNELFLAG_NONE, speed);
 }
 
 /*
