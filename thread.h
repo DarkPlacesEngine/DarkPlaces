c4954d2b50e5d9273b8dab48904ca27ebdee0371
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2011-10-25 23:18:48 +0000

    thread_pthread now uses recursive mutex (THREADRECURSIVE) THREADDEBUG now prints about all Thread_ functions except Init/Shutdown/HasThreads
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@11478 d7cf8633-e32d-0410-b094-e92efae38249

--- thread.h
+++ thread.h
@@ -1,11 +1,21 @@
 #ifndef THREAD_H
 
+// enable Sys_PrintfToTerminal calls on nearly every threading call
 //#define THREADDEBUG
+// use recursive mutex (non-posix) extensions in thread_pthread
+#define THREADRECURSIVE
 
-#define Thread_CreateMutex() (_Thread_CreateMutex(__FILE__, __LINE__))
-#define Thread_DestroyMutex(m) (_Thread_DestroyMutex(m, __FILE__, __LINE__))
-#define Thread_LockMutex(m) (_Thread_LockMutex(m, __FILE__, __LINE__))
-#define Thread_UnlockMutex(m) (_Thread_UnlockMutex(m, __FILE__, __LINE__))
+#define Thread_CreateMutex()              (_Thread_CreateMutex(__FILE__, __LINE__))
+#define Thread_DestroyMutex(m)            (_Thread_DestroyMutex(m, __FILE__, __LINE__))
+#define Thread_LockMutex(m)               (_Thread_LockMutex(m, __FILE__, __LINE__))
+#define Thread_UnlockMutex(m)             (_Thread_UnlockMutex(m, __FILE__, __LINE__))
+#define Thread_CreateCond()               (_Thread_CreateCond(__FILE__, __LINE__))
+#define Thread_DestroyCond(cond)          (_Thread_DestroyCond(cond, __FILE__, __LINE__))
+#define Thread_CondSignal(cond)           (_Thread_CondSignal(cond, __FILE__, __LINE__))
+#define Thread_CondBroadcast(cond)        (_Thread_CondBroadcast(cond, __FILE__, __LINE__))
+#define Thread_CondWait(cond, mutex)      (_Thread_CondWait(cond, mutex, __FILE__, __LINE__))
+#define Thread_CreateThread(fn, data)     (_Thread_CreateThread(fn, data, __FILE__, __LINE__))
+#define Thread_WaitThread(thread, retval) (_Thread_WaitThread(thread, retval, __FILE__, __LINE__))
 
 int Thread_Init(void);
 void Thread_Shutdown(void);
@@ -14,13 +24,13 @@ void *_Thread_CreateMutex(const char *filename, int fileline);
 void _Thread_DestroyMutex(void *mutex, const char *filename, int fileline);
 int _Thread_LockMutex(void *mutex, const char *filename, int fileline);
 int _Thread_UnlockMutex(void *mutex, const char *filename, int fileline);
-void *Thread_CreateCond(void);
-void Thread_DestroyCond(void *cond);
-int Thread_CondSignal(void *cond);
-int Thread_CondBroadcast(void *cond);
-int Thread_CondWait(void *cond, void *mutex);
-void *Thread_CreateThread(int (*fn)(void *), void *data);
-int Thread_WaitThread(void *thread, int retval);
+void *_Thread_CreateCond(const char *filename, int fileline);
+void _Thread_DestroyCond(void *cond, const char *filename, int fileline);
+int _Thread_CondSignal(void *cond, const char *filename, int fileline);
+int _Thread_CondBroadcast(void *cond, const char *filename, int fileline);
+int _Thread_CondWait(void *cond, void *mutex, const char *filename, int fileline);
+void *_Thread_CreateThread(int (*fn)(void *), void *data, const char *filename, int fileline);
+int _Thread_WaitThread(void *thread, int retval, const char *filename, int fileline);
 
 #endif
 
