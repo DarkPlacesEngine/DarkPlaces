8cdb11883916f1662d816e76878fe1b3b30b91f3
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2012-04-01 13:45:55 +0000

    set right include path for two #include commands
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@11782 d7cf8633-e32d-0410-b094-e92efae38249

--- sys_win.c
+++ sys_win.c
@@ -29,7 +29,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "qtypes.h"
 
 #include "quakedef.h"
-#include "errno.h"
+#include <errno.h>
 #include "resource.h"
 #include "conproc.h"
 
