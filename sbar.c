f9b833cfecadba53e1c1732028b9a6f6659d867e
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2012-04-01 13:45:55 +0000

    set right include path for two #include commands
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@11782 d7cf8633-e32d-0410-b094-e92efae38249

--- sbar.c
+++ sbar.c
@@ -20,7 +20,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 // sbar.c -- status bar code
 
 #include "quakedef.h"
-#include "time.h"
+#include <time.h>
 #include "cl_collision.h"
 #include "csprogs.h"
 
