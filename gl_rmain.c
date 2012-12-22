DarkPlacesEngine/DarkPlaces

commit 2048a824c4d5c5d4d031e519dd3a125db35415fd

New shader keyword dptransparentsort (can be "sky", "distance", "hud") which forces one of transparent sort techniques. EF_NODEPTHTEST and RENDER_WORLDOBJECT entity flags have greater priority and override shader-set transparent sort technique. New cvar q3shader_force_terrain_alphaflag which sets TEXF_ALPHA on both vertextextureblend textures (fixes r_glsl_vertextextureblend_usebothalphas).
git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@11822 d7cf8633-e32d-0410-b094-e92efae38249

gl_rmain.c
@@ -227,7 +227,7 @@ cvar_t r_test = {0, "r_test", "0", "internal development use only, leave it alon
 cvar_t r_glsl_saturation = {CVAR_SAVE, "r_glsl_saturation", "1", "saturation multiplier (only working in glsl!)"};
 cvar_t r_glsl_saturation_redcompensate = {CVAR_SAVE, "r_glsl_saturation_redcompensate", "0", "a 'vampire sight' addition to desaturation effect, does compensation for red color, r_glsl_restart is required"};
 
-cvar_t r_glsl_vertextextureblend_usebothalphas = {CVAR_SAVE, "r_glsl_vertextextureblend_usebothalphas", "0", "use both alpha layers on vertex blended surfaces, each alpha layer sets amount of 'blend leak' on another layer."};
+cvar_t r_glsl_vertextextureblend_usebothalphas = {CVAR_SAVE, "r_glsl_vertextextureblend_usebothalphas", "0", "use both alpha layers on vertex blended surfaces, each alpha layer sets amount of 'blend leak' on another layer, requires mod_q3shader_force_terrain_alphaflag on."};
 
 cvar_t r_framedatasize = {CVAR_SAVE, "r_framedatasize", "0.5", "size of renderer data cache used during one frame (for skeletal animation caching, light processing, etc)"};
 
@@ -7357,7 +7357,7 @@ static void R_DrawEntityBBoxes(void)
 		if(PRVM_serveredictedict(edict, viewmodelforclient) != 0)
 			continue;
 		VectorLerp(edict->priv.server->areamins, 0.5f, edict->priv.server->areamaxs, center);
-		R_MeshQueue_AddTransparent(MESHQUEUE_SORT_DISTANCE, center, R_DrawEntityBBoxes_Callback, (entity_render_t *)NULL, i, (rtlight_t *)NULL);
+		R_MeshQueue_AddTransparent(TRANSPARENTSORT_DISTANCE, center, R_DrawEntityBBoxes_Callback, (entity_render_t *)NULL, i, (rtlight_t *)NULL);
 	}
 }
 
@@ -7465,7 +7465,7 @@ void R_DrawNoModel(entity_render_t *ent)
 	vec3_t org;
 	Matrix4x4_OriginFromMatrix(&ent->matrix, org);
 	if ((ent->flags & RENDER_ADDITIVE) || (ent->alpha < 1))
-		R_MeshQueue_AddTransparent((ent->flags & RENDER_NODEPTHTEST) ? MESHQUEUE_SORT_HUD : MESHQUEUE_SORT_DISTANCE, org, R_DrawNoModel_TransparentCallback, ent, 0, rsurface.rtlight);
+		R_MeshQueue_AddTransparent((ent->flags & RENDER_NODEPTHTEST) ? TRANSPARENTSORT_HUD : rsurface.texture->transparentsort, org, R_DrawNoModel_TransparentCallback, ent, 0, rsurface.rtlight);
 	else
 		R_DrawNoModel_TransparentCallback(ent, rsurface.rtlight, 0, NULL);
 }
@@ -10501,7 +10501,7 @@ static void R_ProcessTransparentTextureSurfaceList(int texturenumsurfaces, const
 			center[1] += r_refdef.view.forward[1]*rsurface.entity->transparent_offset;
 			center[2] += r_refdef.view.forward[2]*rsurface.entity->transparent_offset;
 		}
-		R_MeshQueue_AddTransparent((rsurface.texture->currentmaterialflags & MATERIALFLAG_NODEPTHTEST) ? MESHQUEUE_SORT_HUD : ((rsurface.entity->flags & RENDER_WORLDOBJECT) ? MESHQUEUE_SORT_SKY : MESHQUEUE_SORT_DISTANCE), center, R_DrawSurface_TransparentCallback, rsurface.entity, surface - rsurface.modelsurfaces, rsurface.rtlight);
+		R_MeshQueue_AddTransparent((rsurface.entity->flags & RENDER_WORLDOBJECT) ? TRANSPARENTSORT_SKY : (rsurface.texture->currentmaterialflags & MATERIALFLAG_NODEPTHTEST) ? TRANSPARENTSORT_HUD : rsurface.texture->transparentsort, center, R_DrawSurface_TransparentCallback, rsurface.entity, surface - rsurface.modelsurfaces, rsurface.rtlight);
 	}
 }
 
@@ -10748,7 +10748,7 @@ void R_DrawLocs(void)
 	for (loc = cl.locnodes, index = 0;loc;loc = loc->next, index++)
 	{
 		VectorLerp(loc->mins, 0.5f, loc->maxs, center);
-		R_MeshQueue_AddTransparent(MESHQUEUE_SORT_DISTANCE, center, R_DrawLoc_Callback, (entity_render_t *)loc, loc == nearestloc ? -1 : index, NULL);
+		R_MeshQueue_AddTransparent(TRANSPARENTSORT_DISTANCE, center, R_DrawLoc_Callback, (entity_render_t *)loc, loc == nearestloc ? -1 : index, NULL);
 	}
 }
 

model_brush.c
@@ -56,6 +56,8 @@ cvar_t mod_q3shader_default_offsetmapping_bias = {CVAR_SAVE, "mod_q3shader_defau
 cvar_t mod_q3shader_default_polygonfactor = {0, "mod_q3shader_default_polygonfactor", "0", "biases depth values of 'polygonoffset' shaders to prevent z-fighting artifacts"};
 cvar_t mod_q3shader_default_polygonoffset = {0, "mod_q3shader_default_polygonoffset", "-2", "biases depth values of 'polygonoffset' shaders to prevent z-fighting artifacts"};
 cvar_t mod_q3shader_force_addalpha = {0, "mod_q3shader_force_addalpha", "0", "treat GL_ONE GL_ONE (or add) blendfunc as GL_SRC_ALPHA GL_ONE for compatibility with older DarkPlaces releases"};
+cvar_t mod_q3shader_force_terrain_alphaflag = {0, "mod_q3shader_force_terrain_alphaflag", "0", "for multilayered terrain shaders force TEXF_ALPHA flag on both layers"};
+
 cvar_t mod_q1bsp_polygoncollisions = {0, "mod_q1bsp_polygoncollisions", "0", "disables use of precomputed cliphulls and instead collides with polygons (uses Bounding Interval Hierarchy optimizations)"};
 cvar_t mod_collision_bih = {0, "mod_collision_bih", "1", "enables use of generated Bounding Interval Hierarchy tree instead of compiled bsp tree in collision code"};
 cvar_t mod_recalculatenodeboxes = {0, "mod_recalculatenodeboxes", "1", "enables use of generated node bounding boxes based on BSP tree portal reconstruction, rather than the node boxes supplied by the map compiler"};
@@ -98,6 +100,7 @@ void Mod_BrushInit(void)
 	Cvar_RegisterVariable(&mod_q3shader_default_polygonfactor);
 	Cvar_RegisterVariable(&mod_q3shader_default_polygonoffset);
 	Cvar_RegisterVariable(&mod_q3shader_force_addalpha);
+	Cvar_RegisterVariable(&mod_q3shader_force_terrain_alphaflag);
 	Cvar_RegisterVariable(&mod_q1bsp_polygoncollisions);
 	Cvar_RegisterVariable(&mod_collision_bih);
 	Cvar_RegisterVariable(&mod_recalculatenodeboxes);

model_shared.c
@@ -1653,6 +1653,7 @@ extern cvar_t mod_q3shader_default_offsetmapping_bias;
 extern cvar_t mod_q3shader_default_polygonoffset;
 extern cvar_t mod_q3shader_default_polygonfactor;
 extern cvar_t mod_q3shader_force_addalpha;
+extern cvar_t mod_q3shader_force_terrain_alphaflag;
 void Mod_LoadQ3Shaders(void)
 {
 	int j;
@@ -1747,6 +1748,7 @@ void Mod_LoadQ3Shaders(void)
 			shader.specularpowermod = 1;
 			shader.biaspolygonoffset = mod_q3shader_default_polygonoffset.value;
 			shader.biaspolygonfactor = mod_q3shader_default_polygonfactor.value;
+			shader.transparentsort = TRANSPARENTSORT_DISTANCE;
 
 			strlcpy(shader.name, com_token, sizeof(shader.name));
 			if (!COM_ParseToken_QuakeC(&text, false) || strcasecmp(com_token, "{"))
@@ -1999,6 +2001,8 @@ void Mod_LoadQ3Shaders(void)
 						{
 							// multilayer terrain shader or similar
 							shader.textureblendalpha = true;
+							if (mod_q3shader_force_terrain_alphaflag.integer)
+								shader.layers[0].texflags |= TEXF_ALPHA;
 						}
 					}
 
@@ -2009,7 +2013,7 @@ void Mod_LoadQ3Shaders(void)
 						if(layer->blendfunc[0] == GL_ONE && layer->blendfunc[1] == GL_ONE)
 							layer->blendfunc[0] = GL_SRC_ALPHA;
 					}
-
+					
 					layer->texflags = 0;
 					if (layer->alphatest)
 						layer->texflags |= TEXF_ALPHA;
@@ -2244,6 +2248,18 @@ void Mod_LoadQ3Shaders(void)
 						else
 							shader.biaspolygonoffset = 0;
 					}
+				}
+				else if (!strcasecmp(parameter[0], "dptransparentsort") && numparameters >= 2)
+				{
+					shader.textureflags |= Q3TEXTUREFLAG_TRANSPARENTSORT;
+					if (!strcasecmp(parameter[1], "sky"))
+						shader.transparentsort = TRANSPARENTSORT_SKY;
+					else if (!strcasecmp(parameter[1], "distance"))
+						shader.transparentsort = TRANSPARENTSORT_DISTANCE;
+					else if (!strcasecmp(parameter[1], "hud"))
+						shader.transparentsort = TRANSPARENTSORT_HUD;
+					else
+						Con_DPrintf("%s parsing warning: unknown dptransparentsort category \"%s\", or not enough arguments\n", search->filenames[fileindex], parameter[1]);
 				}
 				else if (!strcasecmp(parameter[0], "dprefract") && numparameters >= 5)
 				{
@@ -2484,6 +2500,7 @@ qboolean Mod_LoadTextureFromQ3Shader(texture_t *texture, const char *name, qbool
 			texture->basematerialflags |= MATERIALFLAG_CAMERA;
 		texture->customblendfunc[0] = GL_ONE;
 		texture->customblendfunc[1] = GL_ZERO;
+		texture->transparentsort = shader->transparentsort;
 		if (shader->numlayers > 0)
 		{
 			texture->customblendfunc[0] = shader->layers[0].blendfunc[0];

model_shared.h
@@ -251,6 +251,7 @@ shadowmesh_t;
 #define Q3TEXTUREFLAG_REFLECTION 512
 #define Q3TEXTUREFLAG_WATERSHADER 1024
 #define Q3TEXTUREFLAG_CAMERA 2048
+#define Q3TEXTUREFLAG_TRANSPARENTSORT 4096
 
 #define Q3PATHLENGTH 64
 #define TEXTURE_MAXFRAMES 64
@@ -426,6 +427,12 @@ typedef enum dpoffsetmapping_technique_s
 	OFFSETMAPPING_RELIEF		// relief
 }dpoffsetmapping_technique_t;
 
+typedef enum dptransparentsort_category_e
+{
+	TRANSPARENTSORT_SKY,
+	TRANSPARENTSORT_DISTANCE,
+	TRANSPARENTSORT_HUD,
+}dptransparentsortcategory_t;
 
 typedef struct q3shaderinfo_s
 {
@@ -476,6 +483,9 @@ typedef struct q3shaderinfo_s
 
 	// polygonoffset (only used if Q3TEXTUREFLAG_POLYGONOFFSET)
 	float biaspolygonoffset, biaspolygonfactor;
+
+	// transparent sort category
+	dptransparentsortcategory_t transparentsort;
 
 	// gloss
 	float specularscalemod;
@@ -617,6 +627,9 @@ typedef struct texture_s
 	dpoffsetmapping_technique_t offsetmapping;
 	float offsetscale;
 	float offsetbias;
+
+	// transparent sort category
+	dptransparentsortcategory_t transparentsort;
 
 	// gloss
 	float specularscalemod;

cl_particles.c
@@ -2551,7 +2551,7 @@ void R_DrawDecals (void)
 			continue;
 
 		if (DotProduct(r_refdef.view.origin, decal->normal) > DotProduct(decal->org, decal->normal) && VectorDistance2(decal->org, r_refdef.view.origin) < drawdist2 * (decal->size * decal->size))
-			R_MeshQueue_AddTransparent(MESHQUEUE_SORT_DISTANCE, decal->org, R_DrawDecal_TransparentCallback, NULL, i, NULL);
+			R_MeshQueue_AddTransparent(TRANSPARENTSORT_DISTANCE, decal->org, R_DrawDecal_TransparentCallback, NULL, i, NULL);
 		continue;
 killdecal:
 		decal->typeindex = 0;
@@ -3039,7 +3039,7 @@ void R_DrawParticles (void)
 		{
 		case pt_beam:
 			// beams have no culling
-			R_MeshQueue_AddTransparent(MESHQUEUE_SORT_DISTANCE, p->sortorigin, R_DrawParticle_TransparentCallback, NULL, i, NULL);
+			R_MeshQueue_AddTransparent(TRANSPARENTSORT_DISTANCE, p->sortorigin, R_DrawParticle_TransparentCallback, NULL, i, NULL);
 			break;
 		default:
 			if(cl_particles_visculling.integer)
@@ -3053,7 +3053,7 @@ void R_DrawParticles (void)
 					}
 			// anything else just has to be in front of the viewer and visible at this distance
 			if (DotProduct(p->org, r_refdef.view.forward) >= minparticledist_start && VectorDistance2(p->org, r_refdef.view.origin) < drawdist2 * (p->size * p->size))
-				R_MeshQueue_AddTransparent(MESHQUEUE_SORT_DISTANCE, p->sortorigin, R_DrawParticle_TransparentCallback, NULL, i, NULL);
+				R_MeshQueue_AddTransparent(TRANSPARENTSORT_DISTANCE, p->sortorigin, R_DrawParticle_TransparentCallback, NULL, i, NULL);
 			break;
 		}
 

clvm_cmds.c
@@ -3429,7 +3429,7 @@ void VM_CL_AddPolygonsToMeshQueue (prvm_prog_t *prog)
 	for (i = 0;i < polys->num_triangles;i++)
 	{
 		VectorMAMAM(1.0f / 3.0f, polys->data_vertex3f + 3*polys->data_triangles[i].elements[0], 1.0f / 3.0f, polys->data_vertex3f + 3*polys->data_triangles[i].elements[1], 1.0f / 3.0f, polys->data_vertex3f + 3*polys->data_triangles[i].elements[2], center);
-		R_MeshQueue_AddTransparent(MESHQUEUE_SORT_DISTANCE, center, VM_DrawPolygonCallback, (entity_render_t *)polys, i, NULL);
+		R_MeshQueue_AddTransparent(TRANSPARENTSORT_DISTANCE, center, VM_DrawPolygonCallback, (entity_render_t *)polys, i, NULL);
 	}
 
 	/*polys->num_triangles = 0; // now done after rendering the scene,

gl_rsurf.c
@@ -393,7 +393,7 @@ void R_DrawPortals(void)
 						VectorAdd(center, portal->points[i].position, center);
 					f = ixtable[portal->numpoints];
 					VectorScale(center, f, center);
-					R_MeshQueue_AddTransparent(MESHQUEUE_SORT_DISTANCE, center, R_DrawPortal_Callback, (entity_render_t *)portal, leafnum, rsurface.rtlight);
+					R_MeshQueue_AddTransparent(TRANSPARENTSORT_DISTANCE, center, R_DrawPortal_Callback, (entity_render_t *)portal, leafnum, rsurface.rtlight);
 				}
 			}
 		}
@@ -1535,7 +1535,7 @@ void R_Q1BSP_DrawLight(entity_render_t *ent, int numsurfaces, const int *surface
 						center[1] += r_refdef.view.forward[1]*ent->transparent_offset;
 						center[2] += r_refdef.view.forward[2]*ent->transparent_offset;
 					}
-					R_MeshQueue_AddTransparent((rsurface.entity->flags & RENDER_WORLDOBJECT) ? MESHQUEUE_SORT_SKY : ((rsurface.texture->currentmaterialflags & MATERIALFLAG_NODEPTHTEST) ? MESHQUEUE_SORT_HUD : MESHQUEUE_SORT_DISTANCE), center, R_Q1BSP_DrawLight_TransparentCallback, ent, surface - rsurface.modelsurfaces, rsurface.rtlight);
+					R_MeshQueue_AddTransparent((rsurface.entity->flags & RENDER_WORLDOBJECT) ? TRANSPARENTSORT_SKY : ((rsurface.texture->currentmaterialflags & MATERIALFLAG_NODEPTHTEST) ? TRANSPARENTSORT_HUD : rsurface.texture->transparentsort), center, R_Q1BSP_DrawLight_TransparentCallback, ent, surface - rsurface.modelsurfaces, rsurface.rtlight);
 				}
 				continue;
 			}

meshqueue.c
@@ -10,7 +10,7 @@ typedef struct meshqueue_s
 	int surfacenumber;
 	const rtlight_t *rtlight;
 	float dist;
-	meshqueue_sortcategory_t category;
+	dptransparentsortcategory_t category;
 }
 meshqueue_t;
 
@@ -31,7 +31,7 @@ void R_MeshQueue_BeginScene(void)
 	mqt_viewmaxdist = 0;
 }
 
-void R_MeshQueue_AddTransparent(meshqueue_sortcategory_t category, const vec3_t center, void (*callback)(const entity_render_t *ent, const rtlight_t *rtlight, int numsurfaces, int *surfacelist), const entity_render_t *ent, int surfacenumber, const rtlight_t *rtlight)
+void R_MeshQueue_AddTransparent(dptransparentsortcategory_t category, const vec3_t center, void (*callback)(const entity_render_t *ent, const rtlight_t *rtlight, int numsurfaces, int *surfacelist), const entity_render_t *ent, int surfacenumber, const rtlight_t *rtlight)
 {
 	meshqueue_t *mq;
 	if (mqt_count >= mqt_total || !mqt_array)
@@ -104,14 +104,14 @@ void R_MeshQueue_RenderTransparent(void)
 		switch(mqt->category)
 		{
 		default:
-		case MESHQUEUE_SORT_HUD:
+		case TRANSPARENTSORT_HUD:
 			hashindex = 0;
 			break;
-		case MESHQUEUE_SORT_DISTANCE:
+		case TRANSPARENTSORT_DISTANCE:
 			// this could use a reduced range if we need more categories
 			hashindex = bound(0, (int)(bound(0, mqt->dist - r_transparent_sortmindist.integer, r_transparent_sortmaxdist.integer) * distscale), maxhashindex);
 			break;
-		case MESHQUEUE_SORT_SKY:
+		case TRANSPARENTSORT_SKY:
 			hashindex = maxhashindex;
 			break;
 		}

commit 029564ba9f93c3c075fa2d176c45a3126389c828

fix bug with transparency sorting introduced by vortex in r11822 where .transparentsort was not initialized in a lot of materials, which meant TRANSPARENTSORT_SKY was used rather than TRANSPARENTSORT_DISTANCE
git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@11866 d7cf8633-e32d-0410-b094-e92efae38249

gl_rmain.c
@@ -11867,6 +11867,9 @@ void R_DrawCustomSurface(skinframe_t *skinframe, const matrix4x4_t *texmatrix, i
 	texture.offsetscale = 1;
 	texture.specularscalemod = 1;
 	texture.specularpowermod = 1;
+	texture.transparentsort = TRANSPARENTSORT_DISTANCE;
+	// WHEN ADDING DEFAULTS HERE, REMEMBER TO PUT DEFAULTS IN ALL LOADERS
+	// JUST GREP FOR "specularscalemod = 1".
 
 	surface.texture = &texture;
 	surface.num_triangles = numtriangles;

model_alias.c
@@ -824,6 +824,9 @@ static void Mod_BuildAliasSkinFromSkinFrame(texture_t *texture, skinframe_t *ski
 	texture->supercontents = SUPERCONTENTS_SOLID;
 	if (!(texture->basematerialflags & MATERIALFLAG_BLENDED))
 		texture->supercontents |= SUPERCONTENTS_OPAQUE;
+	texture->transparentsort = TRANSPARENTSORT_DISTANCE;
+	// WHEN ADDING DEFAULTS HERE, REMEMBER TO PUT DEFAULTS IN ALL LOADERS
+	// JUST GREP FOR "specularscalemod = 1".
 }
 
 void Mod_BuildAliasSkinsFromSkinFiles(texture_t *skin, skinfile_t *skinfile, const char *meshname, const char *shadername)

model_brush.c
@@ -1680,6 +1680,9 @@ static void Mod_Q1BSP_LoadTextures(sizebuf_t *sb)
 		tx->offsetbias = 0;
 		tx->specularscalemod = 1;
 		tx->specularpowermod = 1;
+		tx->transparentsort = TRANSPARENTSORT_DISTANCE;
+		// WHEN ADDING DEFAULTS HERE, REMEMBER TO PUT DEFAULTS IN ALL LOADERS
+		// JUST GREP FOR "specularscalemod = 1".
 	}
 
 	if (!sb->cursize)

model_shared.c
@@ -1734,6 +1734,24 @@ void Mod_LoadQ3Shaders(void)
 		while (COM_ParseToken_QuakeC(&text, false))
 		{
 			memset (&shader, 0, sizeof(shader));
+			shader.name[0] = 0;
+			shader.surfaceparms = 0;
+			shader.surfaceflags = 0;
+			shader.textureflags = 0;
+			shader.numlayers = 0;
+			shader.lighting = false;
+			shader.vertexalpha = false;
+			shader.textureblendalpha = false;
+			shader.primarylayer = 0;
+			shader.backgroundlayer = 0;
+			shader.skyboxname[0] = 0;
+			shader.deforms[0].deform = Q3DEFORM_NONE;
+			shader.dpnortlight = false;
+			shader.dpshadow = false;
+			shader.dpnoshadow = false;
+			shader.dpmeshcollisions = false;
+			shader.dpshaderkill = false;
+			shader.dpreflectcube[0] = 0;
 			shader.reflectmin = 0;
 			shader.reflectmax = 1;
 			shader.refractfactor = 1;
@@ -1741,14 +1759,19 @@ void Mod_LoadQ3Shaders(void)
 			shader.reflectfactor = 1;
 			Vector4Set(shader.reflectcolor4f, 1, 1, 1, 1);
 			shader.r_water_wateralpha = 1;
+			shader.r_water_waterscroll[0] = 0;
+			shader.r_water_waterscroll[1] = 0;
 			shader.offsetmapping = (mod_q3shader_default_offsetmapping.value) ? OFFSETMAPPING_DEFAULT : OFFSETMAPPING_OFF;
 			shader.offsetscale = mod_q3shader_default_offsetmapping_scale.value;
 			shader.offsetbias = mod_q3shader_default_offsetmapping_bias.value;
-			shader.specularscalemod = 1;
-			shader.specularpowermod = 1;
 			shader.biaspolygonoffset = mod_q3shader_default_polygonoffset.value;
 			shader.biaspolygonfactor = mod_q3shader_default_polygonfactor.value;
 			shader.transparentsort = TRANSPARENTSORT_DISTANCE;
+			shader.specularscalemod = 1;
+			shader.specularpowermod = 1;
+			shader.rtlightambient = 0;
+			// WHEN ADDING DEFAULTS HERE, REMEMBER TO PUT DEFAULTS IN ALL LOADERS
+			// JUST GREP FOR "specularscalemod = 1".
 
 			strlcpy(shader.name, com_token, sizeof(shader.name));
 			if (!COM_ParseToken_QuakeC(&text, false) || strcasecmp(com_token, "{"))
@@ -2455,8 +2478,8 @@ qboolean Mod_LoadTextureFromQ3Shader(texture_t *texture, const char *name, qbool
 	texture->specularscalemod = 1;
 	texture->specularpowermod = 1; 
 	texture->rtlightambient = 0;
-	// WHEN ADDING DEFAULTS HERE, REMEMBER TO SYNC TO SHADER LOADING ABOVE
-	// HERE, AND Q1BSP LOADING
+	texture->transparentsort = TRANSPARENTSORT_DISTANCE;
+	// WHEN ADDING DEFAULTS HERE, REMEMBER TO PUT DEFAULTS IN ALL LOADERS
 	// JUST GREP FOR "specularscalemod = 1".
 
 	if (shader)

model_shared.h
@@ -491,7 +491,7 @@ typedef struct q3shaderinfo_s
 	float specularscalemod;
 	float specularpowermod;
 
-	// rtlightning ambient addition
+	// rtlighting ambient addition
 	float rtlightambient;
 #define Q3SHADERINFO_COMPARE_END rtlightambient
 }

model_sprite.c
@@ -80,6 +80,9 @@ static void Mod_SpriteSetupTexture(texture_t *texture, skinframe_t *skinframe, q
 	texture->supercontents = SUPERCONTENTS_SOLID;
 	if (!(texture->basematerialflags & MATERIALFLAG_BLENDED))
 		texture->supercontents |= SUPERCONTENTS_OPAQUE;
+	texture->transparentsort = TRANSPARENTSORT_DISTANCE;
+	// WHEN ADDING DEFAULTS HERE, REMEMBER TO PUT DEFAULTS IN ALL LOADERS
+	// JUST GREP FOR "specularscalemod = 1".
 }
 
 extern cvar_t gl_texturecompression_sprites;