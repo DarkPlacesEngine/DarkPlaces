97eea2582d1c1ba27fd2e9983cb64a919a45942c
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2013-02-25 12:28:46 +0000

    Add a cvar to change menu progs filename
    From: Mario <mario.mario@y7mail.com>
    
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@11908 d7cf8633-e32d-0410-b094-e92efae38249
    ::stable-branch::merge=73e441dcaf8470073e3b48755c15085790f2abed

--- menu.h
+++ menu.h
@@ -21,8 +21,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #ifndef MENU_H
 #define MENU_H
 
-#define M_PROG_FILENAME "menu.dat"
-
 enum m_state_e {
 	m_none,
 	m_main,
