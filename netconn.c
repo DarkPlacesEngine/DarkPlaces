07ce9a2d114c71fcd2e8ab14e37a431e2d895f5b
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2013-05-25 14:08:53 +0000

    Implement DSCP for ToS according to RFC2474 / RFC4594
    From: Merlijn Hofstra <merlijn@Merlijns-MacBook-Air.local>
    
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@11951 d7cf8633-e32d-0410-b094-e92efae38249
    ::stable-branch::merge=3368a3b1dd0e71f33bc40c8fe47ad5b2a0255d2f

--- netconn.c
+++ netconn.c
@@ -93,6 +93,7 @@ static cvar_t net_slist_timeout = {0, "net_slist_timeout", "4", "how long to lis
 static cvar_t net_slist_pause = {0, "net_slist_pause", "0", "when set to 1, the server list won't update until it is set back to 0"};
 static cvar_t net_slist_maxtries = {0, "net_slist_maxtries", "3", "how many times to ask the same server for information (more times gives better ping reports but takes longer)"};
 static cvar_t net_slist_favorites = {CVAR_SAVE | CVAR_NQUSERINFOHACK, "net_slist_favorites", "", "contains a list of IP addresses and ports to always query explicitly"};
+static cvar_t net_tos_dscp = {CVAR_SAVE, "net_tos_dscp", "32", "DiffServ Codepoint for network sockets (may need game restart to apply)"};
 static cvar_t gameversion = {0, "gameversion", "0", "version of game data (mod-specific) to be sent to querying clients"};
 static cvar_t gameversion_min = {0, "gameversion_min", "-1", "minimum version of game data (mod-specific), when client and server gameversion mismatch in the server browser the server is shown as incompatible; if -1, gameversion is used alone"};
 static cvar_t gameversion_max = {0, "gameversion_max", "-1", "maximum version of game data (mod-specific), when client and server gameversion mismatch in the server browser the server is shown as incompatible; if -1, gameversion is used alone"};
@@ -1093,6 +1094,9 @@ static int hostport = -1;
 void NetConn_UpdateSockets(void)
 {
 	int i, j;
+
+	// TODO add logic to automatically close sockets if needed
+	LHNET_DefaultDSCP(net_tos_dscp.integer);
 
 	if (cls.state != ca_dedicated)
 	{
@@ -3686,6 +3690,8 @@ void NetConn_Init(void)
 	Cvar_RegisterVariable(&net_slist_maxtries);
 	Cvar_RegisterVariable(&net_slist_favorites);
 	Cvar_RegisterVariable(&net_slist_pause);
+	if(LHNET_DefaultDSCP(-1) >= 0) // register cvar only if supported
+		Cvar_RegisterVariable(&net_tos_dscp);
 	Cvar_RegisterVariable(&net_messagetimeout);
 	Cvar_RegisterVariable(&net_connecttimeout);
 	Cvar_RegisterVariable(&net_connectfloodblockingtimeout);
