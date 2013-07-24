343c50f1d5ba0259ac7b2aa09a1fae80d255982d
nikoli <nikoli@d7cf8633-e32d-0410-b094-e92efae38249>
2013-07-24 16:13:57 +0000

    Add ability to reproduce build
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@11978 d7cf8633-e32d-0410-b094-e92efae38249

--- builddate.c
+++ builddate.c
@@ -2,9 +2,14 @@
 #define STRINGIFY(arg) STRINGIFY2(arg)
 
 extern const char *buildstring;
-const char *buildstring = __TIME__ " " __DATE__
+const char *buildstring =
+#ifndef NO_BUILD_TIMESTAMPS
+__TIME__ " " __DATE__ " "
+#endif
 #ifdef SVNREVISION
-" " STRINGIFY(SVNREVISION)
+STRINGIFY(SVNREVISION)
+#else
+"-"
 #endif
 #ifdef BUILDTYPE
 " " STRINGIFY(BUILDTYPE)
