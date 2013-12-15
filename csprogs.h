52e98268042cbf9d4460898e4fa8ebaa023977e2
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2013-12-15 01:22:30 +0000

    Upgrade CL_VM_InputEvent to using floats rather than ints, this keeps the precision on fractional absolute mouse positions.
    Changed CL_VM_InputEvent passing of relative mouse moves to no longer
    scale by vid_conwidth, vid_conheight as this goes against the intention
    in most cases (where mouse turning or similar is being achieved).
    
    
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@12031 d7cf8633-e32d-0410-b094-e92efae38249

--- csprogs.h
+++ csprogs.h
@@ -89,7 +89,7 @@ void CL_VM_Init(void);
 void CL_VM_ShutDown(void);
 void CL_VM_UpdateIntermissionState(int intermission);
 void CL_VM_UpdateShowingScoresState(int showingscores);
-qboolean CL_VM_InputEvent(int eventtype, int x, int y);
+qboolean CL_VM_InputEvent(int eventtype, float x, float y);
 qboolean CL_VM_ConsoleCommand(const char *cmd);
 void CL_VM_UpdateDmgGlobals(int dmg_take, int dmg_save, vec3_t dmg_origin);
 void CL_VM_UpdateIntermissionState(int intermission);
