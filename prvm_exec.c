91efd4d4260983eae86f644ff1813cc391d3c27e
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2014-01-07 19:22:59 +0000

    Use an explicit jump table for QCVM instruction dispatch.
    Also, duplicate the dispatch into the end of each case for additional
    speedup (each of these branches gets a separate branch prediction slot
    on many CPUs, which pays off quickly).
    
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@12044 d7cf8633-e32d-0410-b094-e92efae38249

--- prvm_exec.c
+++ prvm_exec.c
@@ -716,6 +716,9 @@ static void PRVM_StatementCoverageEvent(prvm_prog_t *prog, mfunction_t *func, in
 	Con_Printf("prvm_coverage: %s just executed a statement at %s for the first time. Coverage: %.2f%%.\n", prog->name, PRVM_WhereAmI(vabuf, sizeof(vabuf), prog, func, statement), prog->statements_covered * 100.0 / prog->numstatements);
 }
 
+#ifdef __GNUC__
+#define HAVE_COMPUTED_GOTOS 1
+#endif
 
 #define OPA ((prvm_eval_t *)&prog->globals.fp[st->operand[0]])
 #define OPB ((prvm_eval_t *)&prog->globals.fp[st->operand[1]])
