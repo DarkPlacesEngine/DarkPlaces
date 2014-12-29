9cd668cb1ffabf1c137ae62c9488f041745c6010
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2014-12-29 14:55:25 +0000

    Use proper Unicode defined characters for the "..." chat continuation sign
    From: Severin Meyer <sev.ch@web.de>
    
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@12112 d7cf8633-e32d-0410-b094-e92efae38249

--- console.c
+++ console.c
@@ -1823,7 +1823,7 @@ void Con_DrawNotify (void)
 
 	if(numChatlines)
 	{
-		Con_DrawNotifyRect(CON_MASK_CHAT, CON_MASK_INPUT, con_chattime.value, x, v, vid_conwidth.value * con_chatwidth.value, height, con_chatsize.value, 0.0, 1.0, (utf8_enable.integer ? "^3\xee\x80\x8c\xee\x80\x8c\xee\x80\x8c " : "^3\014\014\014 ")); // 015 is ·> character in conchars.tga
+		Con_DrawNotifyRect(CON_MASK_CHAT, CON_MASK_INPUT, con_chattime.value, x, v, vid_conwidth.value * con_chatwidth.value, height, con_chatsize.value, 0.0, 1.0, "^3 ... ");
 		v += height;
 	}
 	if (key_dest == key_message)
