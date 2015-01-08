58342ed2afb69d3c0fd3422829b79dd14fa9552b
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2015-01-08 16:27:43 +0000

    If one compiler fails, try another.
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@12126 d7cf8633-e32d-0410-b094-e92efae38249
    ::stable-branch::merge=660001dce93be41b85d5d1e92d4a6afa5de2db33

--- .travis-before_install-xonotic.sh
+++ .travis-before_install-xonotic.sh
@@ -46,8 +46,6 @@ for os in "$@"; do
       )
       ;;
     win32)
-      sudo apt-get install -y \
-        mingw-w64 mingw32- mingw32-binutils-
       ;;
     win64)
       sudo apt-get install -y \
