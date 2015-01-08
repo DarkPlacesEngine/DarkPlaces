2e65079aedcd5ba63ea0df48837a2114bc292408
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2015-01-08 16:47:18 +0000

    Make 64bit binaries the new default in our travis scripts.
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@12128 d7cf8633-e32d-0410-b094-e92efae38249

--- .travis-script-xonotic.sh
+++ .travis-script-xonotic.sh
@@ -71,7 +71,7 @@ for os in "$@"; do
         DP_LINK_ODE=dlopen
         DP_LINK_ZLIB=dlopen'
       maketargets='release'
-      outputs='darkplaces.exe:darkplaces-wgl.exe darkplaces-sdl.exe:darkplaces.exe darkplaces-dedicated.exe:darkplaces-dedicated.exe'
+      outputs='darkplaces.exe:darkplaces-x86-wgl.exe darkplaces-sdl.exe:darkplaces-x86.exe darkplaces-dedicated.exe:darkplaces-x86-dedicated.exe'
       ;;
     win64)
       chroot=
@@ -89,7 +89,7 @@ for os in "$@"; do
         DP_LINK_ODE=dlopen
         DP_LINK_ZLIB=dlopen'
       maketargets='release'
-      outputs='darkplaces.exe:darkplaces-x64-wgl.exe darkplaces-sdl.exe:darkplaces-x64.exe darkplaces-dedicated.exe:darkplaces-x64-dedicated.exe'
+      outputs='darkplaces.exe:darkplaces-wgl.exe darkplaces-sdl.exe:darkplaces.exe darkplaces-dedicated.exe:darkplaces-dedicated.exe'
       ;;
     osx)
       chroot=
