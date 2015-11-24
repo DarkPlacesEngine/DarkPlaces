bb7412376350bf1f1c7b280a4e154e5cc0f3bbbb
divverent <divverent@d7cf8633-e32d-0410-b094-e92efae38249>
2015-11-24 14:58:00 +0000

    Leaktest: store longer allocation origins.
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@12234 d7cf8633-e32d-0410-b094-e92efae38249
    ::stable-branch::merge=68f4284502b457f898002b3b048872ba837be1a4

--- prvm_edict.c
+++ prvm_edict.c
@@ -215,8 +215,8 @@ const char *PRVM_AllocationOrigin(prvm_prog_t *prog)
 	if(prog->leaktest_active)
 	if(prog->depth > 0) // actually in QC code and not just parsing the entities block of a map/savegame
 	{
-		buf = (char *)PRVM_Alloc(128);
-		PRVM_ShortStackTrace(prog, buf, 128);
+		buf = (char *)PRVM_Alloc(256);
+		PRVM_ShortStackTrace(prog, buf, 256);
 	}
 	return buf;
 }
