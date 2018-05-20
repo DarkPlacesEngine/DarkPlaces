ee99334f823e72ed21ed0f14b93fffc36ccf2a0e
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2018-05-20 23:38:27 +0000

    Bugfix: Don't use OpenGL 3.2 core context just yet - more fixes needed first.
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@12426 d7cf8633-e32d-0410-b094-e92efae38249

--- vid_sdl.c
+++ vid_sdl.c
@@ -2050,9 +2050,6 @@ static qboolean VID_InitModeGL(viddef_mode_t *mode)
 	SDL_GL_SetAttribute (SDL_GL_CONTEXT_MAJOR_VERSION, 2);
 	SDL_GL_SetAttribute (SDL_GL_CONTEXT_MINOR_VERSION, 0);
 	SDL_GL_SetAttribute (SDL_GL_RETAINED_BACKING, 1);
-#else
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
 #endif
 
 	video_bpp = mode->bitsperpixel;
