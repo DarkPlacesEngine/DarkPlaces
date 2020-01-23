749072a8f60b88ef37448e224e83944b7096e909
havoc <havoc@d7cf8633-e32d-0410-b094-e92efae38249>
2020-01-23 08:19:11 +0000

    Redesigned TaskQueue to have a queue and distributor model so that threads can keep their own queues of work to do without locking to check for more work.  Tasks are not executed until TaskQueue_WaitForTaskDone calls TaskQueue_DistributeTasks.
    Added a fast path to TaskQueue_DistributeTasks for tasks that are waiting on other tasks to finish first, they are simply dequeued and re-enqueued immediately, and tend to just live on the distributor queue.
    
    TaskQueue thread count is now dynamically adjusted based on tasks being queued per frame, and has a minimum and maximum thread count.
    
    TaskQueue threads now sleep when idle, this may not be ideal, but did substantially lower the cpu usage so that it doesn't make other applications relatively unresponsive like it did before.
    
    Removed taskqueue_task_t->started field and some other unused fields.
    
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@12513 d7cf8633-e32d-0410-b094-e92efae38249

--- taskqueue.h
+++ taskqueue.h
@@ -7,41 +7,32 @@
 
 typedef struct taskqueue_task_s
 {
-	// doubly linked list
-	struct taskqueue_task_s * volatile prev;
-	struct taskqueue_task_s * volatile next;
-
-	// if not NULL, this task must be done before this one will dequeue (faster than simply Yielding immediately)
+	// if not NULL, this task must be done before this one will dequeue (faster than simply calling TaskQueue_Yield immediately)
 	struct taskqueue_task_s *preceding;
 
-	// see TaskQueue_IsDone() to use proper atomics to poll done status
-	volatile int started;
+	// use TaskQueue_IsDone() to poll done status
 	volatile int done;
 
 	// function to call, and parameters for it to use
 	void(*func)(struct taskqueue_task_s *task);
-	void *p[4];
-	size_t i[4];
+	// general purpose parameters
+	void *p[2];
+	size_t i[2];
 
-	// stats:
-	unsigned int yieldcount; // number of times this task has been requeued
+	unsigned int yieldcount; // number of times this task has been requeued - each task counts only once for purposes of tasksperthread averaging
 }
 taskqueue_task_t;
 
-// immediately execute any pending tasks if threading is disabled (or if force is true)
-// TRY NOT TO USE THIS IF POSSIBLE - poll task->done instead.
-void TaskQueue_Execute(qboolean force);
-
-// queue the tasks to be executed, or executes them immediately if threading is disabled.
+// queue the tasks to be executed, but does not start them (until TaskQueue_WaitforTaskDone is called)
 void TaskQueue_Enqueue(int numtasks, taskqueue_task_t *tasks);
 
 // if the task can not be completed due yet to preconditions, just enqueue it again...
 void TaskQueue_Yield(taskqueue_task_t *t);
 
-// polls for status of task and returns the result immediately - use this instead of checking ->done directly, as this uses atomics
+// polls for status of task and returns the result, does not cause tasks to be executed (see TaskQueue_WaitForTaskDone for that)
 qboolean TaskQueue_IsDone(taskqueue_task_t *t);
 
-// polls for status of task and waits for it to be done
+// triggers execution of queued tasks, and waits for the specified task to be done
 void TaskQueue_WaitForTaskDone(taskqueue_task_t *t);
 
 // convenience function for setting up a task structure.  Does not do the Enqueue, just fills in the struct.
