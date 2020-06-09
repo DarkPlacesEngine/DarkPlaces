28f59a2be5974b041d5f51daad3e5158296cf14e
cloudwalk <cloudwalk@d7cf8633-e32d-0410-b094-e92efae38249>
2020-06-09 14:58:45 +0000

    Rearrange cvar help to fix compatibility with some regex parsing of cvar help
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@12660 d7cf8633-e32d-0410-b094-e92efae38249

--- cvar.c
+++ cvar.c
@@ -268,13 +268,11 @@ const char **Cvar_CompleteBuildList(cvar_state_t *cvars, const char *partial, in
 
 void Cvar_PrintHelp(cvar_t *cvar, const char *name, qboolean full)
 {
-	Con_Printf("^3%s^7", name);
+	Con_Printf("^3%s^7 is \"%s\" [\"%s\"]", name, ((cvar->flags & CVAR_PRIVATE) ? "********"/*hunter2*/ : cvar->string), cvar->defstring);
 	if (strcmp(cvar->name, name))
-		Con_Printf(" (now ^3%s^7)", cvar->name);
-	Con_Printf(" is \"%s\" [\"%s\"] ", ((cvar->flags & CVAR_PRIVATE) ? "********"/*hunter2*/ : cvar->string), cvar->defstring);
-	
+		Con_Printf(" (also ^3%s^7)", cvar->name);
 	if (full)
-		Con_Printf("%s", cvar->description);
+		Con_Printf(" %s", cvar->description);
 	Con_Printf("\n");
 }
 
