613a98cfecc2942455064e4b43f4e3f2a81a3e02
Cloudwalk <cloudwalk@icculus.org>
2020-06-15 02:11:31 +0000

    Play chat sound defined by cvars rather than hardcoded strings

--- client.h
+++ client.h
@@ -1242,7 +1242,7 @@ typedef struct client_state_s
 	sfx_t *sfx_ric3;
 	sfx_t *sfx_r_exp3;
 	// indicates that the file "sound/misc/talk2.wav" was found (for use by team chat messages)
-	qboolean foundtalk2wav;
+	qboolean foundteamchatsound;
 
 // refresh related state
 
