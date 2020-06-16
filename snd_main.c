ac8080688588aba1ab7934f7ba0c2ffea0c8eb91
cloudwalk <cloudwalk@d7cf8633-e32d-0410-b094-e92efae38249>
2020-06-16 13:41:46 +0000

    Implement XMP module sound support
    This implements support for libxmp in the engine. It will dlopen by
    default and is therefore not a strict dependency.
    
    Implementation by nyov: https://gitlab.com/xonotic/darkplaces/-/merge_requests/8
    Updated for darkplaces trunk/master by nico: https://gitlab.com/xonotic/darkplaces/-/merge_requests/88
    
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@12682 d7cf8633-e32d-0410-b094-e92efae38249

--- snd_main.c
+++ snd_main.c
@@ -23,6 +23,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include "snd_main.h"
 #include "snd_ogg.h"
+#include "snd_xmp.h"
 #include "csprogs.h"
 #include "cl_collision.h"
 #include "cdaudio.h"
@@ -800,6 +801,9 @@ void S_Init(void)
 	memset(channels, 0, MAX_CHANNELS * sizeof(channel_t));
 
 	OGG_OpenLibrary ();
+#ifdef USEXMP
+	XMP_OpenLibrary ();
+#endif
 }
 
 
@@ -813,6 +817,9 @@ Shutdown and free all resources
 void S_Terminate (void)
 {
 	S_Shutdown ();
+#ifdef USEXMP
+	XMP_CloseLibrary ();
+#endif
 	OGG_CloseLibrary ();
 
 	// Free all SFXs
