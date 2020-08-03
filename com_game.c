d8710192a6d58236028449e01d6e8c2947d322ac
cloudwalk <cloudwalk@d7cf8633-e32d-0410-b094-e92efae38249>
2020-08-03 19:25:44 +0000

    com_game: Add missing copyright header
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@12883 d7cf8633-e32d-0410-b094-e92efae38249

--- com_game.c
+++ com_game.c
@@ -1,3 +1,26 @@
+/*
+Copyright (C) 1996-1997 Id Software, Inc.
+Copyright (C) 2000-2020 Ashley Rose "LadyHavoc" Hale
+Copyright (C) 2020 David "Cloudwalk" Knapp
+Copyright (C) 2020 Kristus <kristustf@gmail.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
+*/
+
 #include "quakedef.h"
 
 // Game mods
