48c0ae83baa25ba0171f3b8edcb1bff468976ca2
Cloudwalk <cloudwalk@icculus.org>
2020-09-17 17:50:43 +0000

    quakedef: Include thread.h, and include threads.h in thread.h

--- host.c
+++ host.c
@@ -30,7 +30,6 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "sv_demo.h"
 #include "snd_main.h"
 #include "taskqueue.h"
-#include "thread.h"
 #include "utf8lib.h"
 
 /*
