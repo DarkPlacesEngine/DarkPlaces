f40ee385f633869bc533b7f1fc8412685476fefd
cloudwalk <cloudwalk@d7cf8633-e32d-0410-b094-e92efae38249>
2020-09-29 19:19:02 +0000

    client: Move host prototypes to host.h
    git-svn-id: svn://svn.icculus.org/twilight/trunk/darkplaces@12964 d7cf8633-e32d-0410-b094-e92efae38249

--- host.h
+++ host.h
@@ -40,9 +40,11 @@ double Host_Frame(double time);
 void Host_Shutdown(void);
 void Host_StartVideo(void);
 void Host_Error(const char *error, ...) DP_FUNC_PRINTF(1) DP_FUNC_NORETURN;
-void Host_NoOperation_f(cmd_state_t *cmd);
 void Host_LockSession(void);
 void Host_UnlockSession(void);
 void Host_AbortCurrentFrame(void);
+void Host_SaveConfig(void);
+void Host_LoadConfig_f(cmd_state_t *cmd);
+void Host_NoOperation_f(cmd_state_t *cmd);
 
 #endif
