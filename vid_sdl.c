DarkPlacesEngine/DarkPlaces

commit 08a9d0c21e225d5e02a25c7565c0d90801ba4028

Simplify VID_SetMouse() calling logic
Signed-off-by: bones_was_here <bones_was_here@xonotic.au>

vid_sdl.c
@@ -371,7 +371,7 @@ qbool VID_ShowingKeyboard(void)
 	return SDL_IsTextInputActive() != 0;
 }
 
-void VID_SetMouse(qbool fullscreengrab, qbool relative, qbool hidecursor)
+void VID_SetMouse(qbool relative, qbool hidecursor)
 {
 #ifndef DP_MOBILETOUCH
 #ifdef MACOSX
@@ -384,7 +384,7 @@ void VID_SetMouse(qbool fullscreengrab, qbool relative, qbool hidecursor)
 		vid_usingmouse = relative;
 		cl_ignoremousemoves = 2;
 		vid_usingmouse_relativeworks = SDL_SetRelativeMouseMode(relative ? SDL_TRUE : SDL_FALSE) == 0;
-//		Con_Printf("VID_SetMouse(%i, %i, %i) relativeworks = %i\n", (int)fullscreengrab, (int)relative, (int)hidecursor, (int)vid_usingmouse_relativeworks);
+//		Con_Printf("VID_SetMouse(%i, %i) relativeworks = %i\n", (int)relative, (int)hidecursor, (int)vid_usingmouse_relativeworks);
 #ifdef MACOSX
 		if(relative)
 		{
@@ -1655,7 +1655,7 @@ qbool VID_InitMode(viddef_mode_t *mode)
 void VID_Shutdown (void)
 {
 	VID_EnableJoystick(false);
-	VID_SetMouse(false, false, false);
+	VID_SetMouse(false, false);
 
 	SDL_DestroyWindow(window);
 	window = NULL;

cl_screen.c
@@ -2326,16 +2326,12 @@ void CL_UpdateScreen(void)
 
 	qglFlush(); // ensure that the commands are submitted to the GPU before we do other things
 
-	if (!vid_activewindow)
-		VID_SetMouse(false, false, false);
-	else if (key_consoleactive)
-		VID_SetMouse(vid.fullscreen, false, false);
-	else if (key_dest == key_menu_grabbed)
-		VID_SetMouse(true, vid_mouse.integer && !in_client_mouse && !vid_touchscreen.integer, !vid_touchscreen.integer);
-	else if (key_dest == key_menu || scr_loading)
-		VID_SetMouse(vid.fullscreen, vid_mouse.integer && !in_client_mouse && !vid_touchscreen.integer, !vid_touchscreen.integer);
+	if (!vid_activewindow || key_consoleactive)
+		VID_SetMouse(false, false);
+	else if (key_dest == key_menu || key_dest == key_menu_grabbed || scr_loading)
+		VID_SetMouse(vid_mouse.integer && !in_client_mouse && !vid_touchscreen.integer, !vid_touchscreen.integer);
 	else
-		VID_SetMouse(vid.fullscreen, vid_mouse.integer && !cl.csqc_wantsmousemove && cl_prydoncursor.integer <= 0 && (!cls.demoplayback || cl_demo_mousegrab.integer) && !vid_touchscreen.integer, !vid_touchscreen.integer);
+		VID_SetMouse(vid_mouse.integer && !cl.csqc_wantsmousemove && cl_prydoncursor.integer <= 0 && (!cls.demoplayback || cl_demo_mousegrab.integer) && !vid_touchscreen.integer, !vid_touchscreen.integer);
 
 	VID_Finish();
 }

vid.h
@@ -223,7 +223,7 @@ qbool VID_HasScreenKeyboardSupport(void);
 void VID_ShowKeyboard(qbool show);
 qbool VID_ShowingKeyboard(void);
 
-void VID_SetMouse (qbool fullscreengrab, qbool relative, qbool hidecursor);
+void VID_SetMouse(qbool relative, qbool hidecursor);
 void VID_Finish (void);
 
 void VID_Restart_f(struct cmd_state_s *cmd);

vid_null.c
@@ -51,7 +51,7 @@ static void InitSig(void)
 }
 #endif
 
-void VID_SetMouse (qbool fullscreengrab, qbool relative, qbool hidecursor)
+void VID_SetMouse(qbool relative, qbool hidecursor)
 {
 }
 

commit 6e66fcbea049c74b8d086ac2cbd9246f0a4afd62

Fix compilation on macOS
It has been broken since 08a9d0c21e225d5e02a25c7565c0d90801ba4028.

Signed-off-by: kiwixz <kiwixz@outlook.com>

vid_sdl.c
@@ -378,7 +378,7 @@ void VID_SetMouse(qbool relative, qbool hidecursor)
 #ifdef MACOSX
 	if(relative)
 		if(vid_usingmouse && (vid_usingnoaccel != !!apple_mouse_noaccel.integer))
-			VID_SetMouse(false, false, false); // ungrab first!
+			VID_SetMouse(false, false); // ungrab first!
 #endif
 	if (vid_usingmouse != relative)
 	{