6601a949ecaff1cc10e173289de007858c8d71e2
bones_was_here <bones_was_here@xonotic.au>
2023-07-21 18:28:04 +1000

    Add cvar mod_q1bsp_zero_hullsize_cutoff for games using "big" projectiles on Q1BSP
    Signed-off-by: bones_was_here <bones_was_here@xonotic.au>

--- cmd.c
+++ cmd.c
@@ -804,6 +804,7 @@ static void Cmd_Exec(cmd_state_t *cmd, const char *filename)
 "csqc_polygons_defaultmaterial_nocullface 1\n"
 "con_chatsound_team_mask 13\n"
 "sv_gameplayfix_customstats 1\n"
+"mod_q1bsp_zero_hullsize_cutoff 8.03125\n"
 				);
 			break;
 		// Steel Storm: Burning Retribution csqc misinterprets CSQC_InputEvent if type is a value other than 0 or 1
