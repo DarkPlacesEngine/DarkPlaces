DarkPlacesEngine/DarkPlaces

commit 6b21c467a83808e3becf61afe7b67a88f870b502

cbuf: prepend keybind commands instead of appending to skip waits
This prevents user inputs being delayed by `wait` commands that other
sources may have added to the buffer.

Signed-off-by: bones_was_here <bones_was_here@xonotic.au>

keys.c
@@ -1972,14 +1972,12 @@ Key_Event (int key, int ascii, qbool down)
 			{
 				// button commands add keynum as a parm
 				if (bind[0] == '+')
-					Cbuf_AddText (cmd, va(vabuf, sizeof(vabuf), "%s %i\n", bind, key));
+					Cbuf_InsertText(cmd, va(vabuf, sizeof(vabuf), "%s %i\n", bind, key));
 				else
-				{
-					Cbuf_AddText (cmd, bind);
-					Cbuf_AddText (cmd, "\n");
-				}
-			} else if(bind[0] == '+' && !down && keydown[key] == 0)
-				Cbuf_AddText(cmd, va(vabuf, sizeof(vabuf), "-%s %i\n", bind + 1, key));
+					Cbuf_InsertText(cmd, bind);
+			}
+			else if(bind[0] == '+' && !down && keydown[key] == 0)
+				Cbuf_InsertText(cmd, va(vabuf, sizeof(vabuf), "-%s %i\n", bind + 1, key));
 		}
 		return;
 	}
@@ -2010,7 +2008,7 @@ Key_Event (int key, int ascii, qbool down)
 	{
 		if (down && con_closeontoggleconsole.integer && bind && !strncmp(bind, "toggleconsole", strlen("toggleconsole")) && ascii != STRING_COLOR_TAG)
 		{
-			Cbuf_AddText(cmd, "toggleconsole\n");  // Deferred to next frame so we're not sending the text event to the console.
+			Cbuf_InsertText(cmd, "toggleconsole\n");  // Deferred to next frame so we're not sending the text event to the console.
 			tbl_keydest[key] = key_void; // key release should go nowhere (especially not to key_menu or key_game)
 			return;
 		}
@@ -2053,14 +2051,12 @@ Key_Event (int key, int ascii, qbool down)
 				{
 					// button commands add keynum as a parm
 					if (bind[0] == '+')
-						Cbuf_AddText (cmd, va(vabuf, sizeof(vabuf), "%s %i\n", bind, key));
+						Cbuf_InsertText(cmd, va(vabuf, sizeof(vabuf), "%s %i\n", bind, key));
 					else
-					{
-						Cbuf_AddText (cmd, bind);
-						Cbuf_AddText (cmd, "\n");
-					}
-				} else if(bind[0] == '+' && !down && keydown[key] == 0)
-					Cbuf_AddText(cmd, va(vabuf, sizeof(vabuf), "-%s %i\n", bind + 1, key));
+						Cbuf_InsertText(cmd, bind);
+				}
+				else if(bind[0] == '+' && !down && keydown[key] == 0)
+					Cbuf_InsertText(cmd, va(vabuf, sizeof(vabuf), "-%s %i\n", bind + 1, key));
 			}
 			break;
 		default:

commit 1777de7aa1438b737ee71d20fb9fe6b8c32fafce

Fix bug where +binds could get latched at low FPS
This was introduced in 6b21c467a83808e3becf61afe7b67a88f870b502 which
didn't ensure the -bind would be executed after the +bind when the
keydown and keyup events are processed in the same frame.

Fixes https://github.com/DarkPlacesEngine/darkplaces/issues/106

Signed-off-by: bones_was_here <bones_was_here@xonotic.au>

keys.c
@@ -1971,13 +1971,15 @@ Key_Event (int key, int ascii, qbool down)
 			if(keydown[key] == 1 && down)
 			{
 				// button commands add keynum as a parm
+				// prepend to avoid delays from `wait` commands added by other sources
 				if (bind[0] == '+')
 					Cbuf_InsertText(cmd, va(vabuf, sizeof(vabuf), "%s %i\n", bind, key));
 				else
 					Cbuf_InsertText(cmd, bind);
 			}
 			else if(bind[0] == '+' && !down && keydown[key] == 0)
-				Cbuf_InsertText(cmd, va(vabuf, sizeof(vabuf), "-%s %i\n", bind + 1, key));
+				// append -bind to ensure it's after the +bind in case they arrive in the same frame
+				Cbuf_AddText(cmd, va(vabuf, sizeof(vabuf), "-%s %i\n", bind + 1, key));
 		}
 		return;
 	}
@@ -2050,13 +2052,15 @@ Key_Event (int key, int ascii, qbool down)
 				if(keydown[key] == 1 && down)
 				{
 					// button commands add keynum as a parm
+					// prepend to avoid delays from `wait` commands added by other sources
 					if (bind[0] == '+')
 						Cbuf_InsertText(cmd, va(vabuf, sizeof(vabuf), "%s %i\n", bind, key));
 					else
 						Cbuf_InsertText(cmd, bind);
 				}
 				else if(bind[0] == '+' && !down && keydown[key] == 0)
-					Cbuf_InsertText(cmd, va(vabuf, sizeof(vabuf), "-%s %i\n", bind + 1, key));
+					// append -bind to ensure it's after the +bind in case they arrive in the same frame
+					Cbuf_AddText(cmd, va(vabuf, sizeof(vabuf), "-%s %i\n", bind + 1, key));
 			}
 			break;
 		default: