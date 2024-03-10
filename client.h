daftmugi/wrath-darkplaces

Add underwater sound filter
Enabled by default. Use 'snd_waterfx 0' to disable it.

Adapted code from Ironwail so that it works in this engine.

client.h
@@ -1469,6 +1469,9 @@ typedef struct client_state_s
 
 	// used by EntityState5_ReadUpdate
 	skeleton_t *engineskeletonobjects;
+
+	// used by underwater sound filter (snd_waterfx)
+	qboolean view_underwater;
 }
 client_state_t;
 

DarkPlacesEngine/DarkPlaces

Cherry-picked from daftmugi/wrath-darkplaces

client.h
@@ -1135,6 +1135,9 @@ typedef struct client_state_s
 
 	// used by EntityState5_ReadUpdate
 	skeleton_t *engineskeletonobjects;
+
+	// used by underwater sound filter (snd_waterfx)
+	qbool view_underwater;
 }
 client_state_t;
 