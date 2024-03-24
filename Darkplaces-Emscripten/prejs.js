5db0b680696be67dd0799d8b86e8ac0811a1b26c
Lock l00p <hakzstudios@gmail.com>
2024-03-24 02:50:40 +0000

    Make the user only be able to start darkplaces after everything is loaded

--- Darkplaces-Emscripten/prejs.js
+++ Darkplaces-Emscripten/prejs.js
@@ -1,8 +1,5 @@
-//current command in ascii decimal
-let currentcmd = [0,0,0] 
-let currentfile = "";
-const sleep = ms => new Promise(r => setTimeout(r,ms));
 Module['print'] = function(text){console.log(text);}
+//and this runs right after calling main but before the C code actually runs
 Module['preRun'].push(function()
 {
     
@@ -15,40 +12,42 @@ Module['preRun'].push(function()
     
 })
 Module['noInitialRun'] = true
-//Event listener is required as typical web browsers won't let you load IndexedDB before the user
-//interacts with the page. Instead, it would just crash.
-document.addEventListener('click', (ev) => {
-    console.log("event is captured only once.");
-    args = ["-basedir"]
-    if(window.location.href.indexOf("file://") > -1)
-    {
-        args.push("/home/web_user")
-        try 
+//The function is defined here because Module.onRuntimeInitialized runs after loading everything so this
+//ensures the end user can't start the game before files load
+Module['onRuntimeInitialized'] = function(){
+    //Event listener is required as typical web browsers won't let you load IndexedDB before the user
+    //interacts with the page. Instead, it would just crash.
+    document.addEventListener('click', (ev) => {
+        console.log("event is captured only once.");
+        args = ["-basedir"]
+        if(window.location.href.indexOf("file://") > -1)
         {
-            args = args.concat(prompt("Enter command line arguments").split(" "))
-        } catch (error) 
-        {
-            console.log("Error: ",error);
-            console.log("Failed to concat extra arguments (likely passed nothing for the argument)")
-        }
-        
+            args.push("/home/web_user")
+            try 
+            {
+                args = args.concat(prompt("Enter command line arguments").split(" "))
+            } catch (error) 
+            {
+                console.log("Error: ",error);
+                console.log("Failed to concat extra arguments (likely passed nothing for the argument)")
+            }
+            
 
-    } else
-    {
-        args.push("/game")
-        parms = new URLSearchParams(window.location.search);
-        try 
+        } else
         {
-            args = args.concat(parms.get("args").split(" "))
-        } catch (error) 
-        {
-            console.log("Error: ",error);
-            console.log("Failed to concat extra arguments (likely passed nothing for the argument)")
+            args.push("/game")
+            parms = new URLSearchParams(window.location.search);
+            try 
+            {
+                args = args.concat(parms.get("args").split(" "))
+            } catch (error) 
+            {
+                console.log("Error: ",error);
+                console.log("Failed to concat extra arguments (likely passed nothing for the argument)")
+            }
+            
         }
+        FS.syncfs(true,function(){Module.callMain(args);});
         
-    }
-    FS.syncfs(true,function(){Module.callMain(args);});
-    
-  }, { once: true });
-
-  
+    }, { once: true });
+}
