DarkPlacesEngine/DarkPlaces

commit 01642af37128bffc1accbf2e92cb2428ea570720

sys: optimise printing to stdout
Signed-off-by: bones_was_here <bones_was_here@xonotic.au>

sys_shared.c
@@ -596,7 +596,7 @@ STDIO
 ===============================================================================
 */
 
-void Sys_Print(const char *text)
+void Sys_Print(const char *text, size_t textlen)
 {
 #ifdef __ANDROID__
 	if (developer.integer > 0)
@@ -618,7 +618,7 @@ void Sys_Print(const char *text)
   #endif
 		while(*text)
 		{
-			fs_offset_t written = (fs_offset_t)write(sys.outfd, text, (int)strlen(text));
+			fs_offset_t written = (fs_offset_t)write(sys.outfd, text, textlen);
 			if(written <= 0)
 				break; // sorry, I cannot do anything about this error - without an output
 			text += written;
@@ -632,17 +632,18 @@ void Sys_Print(const char *text)
 #endif
 }
 
-/// for the console to report failures inside Con_Printf()
 void Sys_Printf(const char *fmt, ...)
 {
 	va_list argptr;
 	char msg[MAX_INPUTLINE];
+	int msglen;
 
 	va_start(argptr,fmt);
-	dpvsnprintf(msg,sizeof(msg),fmt,argptr);
+	msglen = dpvsnprintf(msg, sizeof(msg), fmt, argptr);
 	va_end(argptr);
 
-	Sys_Print(msg);
+	if (msglen >= 0)
+		Sys_Print(msg, msglen);
 }
 
 /// Reads a line from POSIX stdin or the Windows console

cl_screen.c
@@ -1224,19 +1224,13 @@ static void SCR_CaptureVideo_VideoFrame(int newframestepframenum)
 	cls.capturevideo.videoframes(newframestepframenum - cls.capturevideo.framestepframe);
 	cls.capturevideo.framestepframe = newframestepframenum;
 
-	if(cl_capturevideo_printfps.integer)
+	if(cl_capturevideo_printfps.integer && host.realtime > cls.capturevideo.lastfpstime + 1)
 	{
-		char buf[80];
-		double t = host.realtime;
-		if(t > cls.capturevideo.lastfpstime + 1)
-		{
-			double fps1 = (cls.capturevideo.frame - cls.capturevideo.lastfpsframe) / (t - cls.capturevideo.lastfpstime + 0.0000001);
-			double fps  = (cls.capturevideo.frame                                ) / (t - cls.capturevideo.starttime   + 0.0000001);
-			dpsnprintf(buf, sizeof(buf), "capturevideo: (%.1fs) last second %.3ffps, total %.3ffps\n", cls.capturevideo.frame / cls.capturevideo.framerate, fps1, fps);
-			Sys_Print(buf);
-			cls.capturevideo.lastfpstime = t;
-			cls.capturevideo.lastfpsframe = cls.capturevideo.frame;
-		}
+		double fps1 = (cls.capturevideo.frame - cls.capturevideo.lastfpsframe) / (host.realtime - cls.capturevideo.lastfpstime + 0.0000001);
+		double fps  = (cls.capturevideo.frame                                ) / (host.realtime - cls.capturevideo.starttime   + 0.0000001);
+		Sys_Printf("capturevideo: (%.1fs) last second %.3ffps, total %.3ffps\n", cls.capturevideo.frame / cls.capturevideo.framerate, fps1, fps);
+		cls.capturevideo.lastfpstime = host.realtime;
+		cls.capturevideo.lastfpsframe = cls.capturevideo.frame;
 	}
 }
 

console.c
@@ -1356,12 +1356,12 @@ void Con_MaskPrint(int additionalmask, const char *msg)
 						*out++ = '[';
 						*out++ = 'm';
 					}
-					*out++ = 0;
-					Sys_Print(printline);
+					*out = '\0';
+					Sys_Print(printline, out - printline);
 				}
 				else if(sys_colortranslation.integer == 2) // Quake
 				{
-					Sys_Print(line);
+					Sys_Print(line, index);
 				}
 				else // strip
 				{
@@ -1411,8 +1411,8 @@ void Con_MaskPrint(int additionalmask, const char *msg)
 								break;
 						}
 					}
-					*out++ = 0;
-					Sys_Print(printline);
+					*out = '\0';
+					Sys_Print(printline, out - printline);
 				}
 			}
 			// empty the line buffer

console.h
@@ -46,7 +46,7 @@ void Con_Shutdown (void);
 void Con_DrawConsole (int lines);
 
 /// Prints to a chosen console target
-void Con_MaskPrint(int mask, const char *msg);
+void Con_MaskPrint(int additionalmask, const char *msg);
 
 // Prints to a chosen console target
 void Con_MaskPrintf(int mask, const char *fmt, ...) DP_FUNC_PRINTF(2);

sys.h
@@ -208,8 +208,9 @@ char *Sys_TimeString(const char *timeformat);
 void Sys_Error (const char *error, ...) DP_FUNC_PRINTF(1) DP_FUNC_NORETURN;
 
 /// (may) output text to terminal which launched program
-void Sys_Print(const char *text);
-/// for the console to report failures inside Con_Printf()
+/// textlen excludes any (optional) \0 terminator
+void Sys_Print(const char *text, size_t textlen);
+/// used to report failures inside Con_Printf()
 void Sys_Printf(const char *fmt, ...);
 
 /// INFO: This is only called by Host_Shutdown so we dont need testing for recursion

commit e0ae55262c874a8846d6f882fa2416c0ed62d5cd

com: fix loop/crash in dpvsnprintf() error handling
Somehow I didn't notice Sys_Printf() calls dpvsnprintf(), see
21fa9010ae6de8792f36cca08cb77aaa95d4b928.

Also fixes a minor bug where Sys_Print() could output more bytes than it
should, introduced in 01642af37128bffc1accbf2e92cb2428ea570720.

Signed-off-by: bones_was_here <bones_was_here@xonotic.au>

common.c
@@ -1018,12 +1018,27 @@ int dpvsnprintf (char *buffer, size_t buffersize, const char *format, va_list ar
 #endif
 	if (result < 0 || (size_t)result >= buffersize)
 	{
-		buffer[buffersize - 1] = '\0';
-		// we could be inside Con_Printf
+		// _vsnprintf_s returns -1 on error and on truncation (indistinguishable),
+		// vsnprintf returns negative on error and the desired strlen on truncation.
+		buffer[buffersize - 1] = '\0'; // should be unnecessary, but just in case
+		// Basic stdout only: we could be inside Con_Printf, Sys_Printf calls dpvsnprintf,
+		// Windows console doesn't support colours.
 		if (result < 0)
-			Sys_Printf("dpvsnprintf: output error, buffer size %lu\n", (unsigned long)buffersize);
+			Sys_Print("dpvsnprintf: output error!\n", 27);
 		else
-			Sys_Printf("dpvsnprintf: truncated to %lu bytes: \"%s\"\n", (unsigned long)buffersize - 1, buffer);
+		{
+			char msg[MAX_INPUTLINE];
+#if _MSC_VER >= 1400
+			result = _snprintf_s(msg, sizeof(msg), _TRUNCATE, "dpvsnprintf: truncated to %lu bytes: \"%s\"\n", (unsigned long)buffersize - 1, buffer);
+#else
+			result = snprintf(msg, sizeof(msg), "dpvsnprintf: truncated to %lu bytes: \"%s\"\n", (unsigned long)buffersize - 1, buffer);
+#endif
+			if (result > 0)
+			{
+				msg[sizeof(msg) - 1] = '\n'; // may have been lost in truncation
+				Sys_Print(msg, min((size_t)result, sizeof(msg)));
+			}
+		}
 		return -1;
 	}
 

sys_shared.c
@@ -629,12 +629,13 @@ void Sys_Print(const char *text, size_t textlen)
   #else
     #define write _write
   #endif
-		while(*text)
+		while(*text && textlen)
 		{
 			fs_offset_t written = (fs_offset_t)write(sys.outfd, text, textlen);
 			if(written <= 0)
 				break; // sorry, I cannot do anything about this error - without an output
 			text += written;
+			textlen -= written;
 		}
   #ifndef WIN32
 		if (sys_stdout_blocks.integer)