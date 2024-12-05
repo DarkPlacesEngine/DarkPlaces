c3d3373b6a3ba62a3a15b7f675884c4266e305e3
k9er <k9wolf@pm.me>
2024-12-05 01:01:20 -0800

    Prevent VM_rint outputting negative zero
    As the name suggests, rounding to an int should output a value that can actually be stored in an int, which negative zero can't.

--- prvm_cmds.c
+++ prvm_cmds.c
@@ -1511,10 +1511,13 @@ void VM_rint(prvm_prog_t *prog)
 	VM_SAFEPARMCOUNT(1,VM_rint);
 
 	f = PRVM_G_FLOAT(OFS_PARM0);
-	if (f > 0)
+	if (f >= 0)
 		PRVM_G_FLOAT(OFS_RETURN) = floor(f + 0.5);
 	else
-		PRVM_G_FLOAT(OFS_RETURN) = ceil(f - 0.5);
+	{
+		f = ceil(f - 0.5);
+		PRVM_G_FLOAT(OFS_RETURN) = f == -0.0 ? 0.0 : f;
+	}
 }
 
 /*
